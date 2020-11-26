07424e576228c25dee49ea5629d8cedfe3b329d4
PaulHuwe <phuwe@stsci.edu>
2020-11-25 19:10:15 -0500

    Added initial DarkModel implementation and dark current file schema. Added gain_factor to the exposure object in the core schema.

---
+++ romancal/datamodels/dark.py
@@ -0,0 +1,30 @@
+from .referencefile import ReferenceFileModel
+# Saved for DQ code implementation
+# from .dynamicdq import dynamic_mask
+
+class DarkModel(ReferenceFileModel):
+    """
+    A data model for dark reference files.
+
+    Parameters
+    __________
+    data : numpy float32 array
+         Dark current array
+
+    dq : numpy uint16 array
+         2-D data quality array for all planes
+
+    err : numpy float32 array
+         Error array
+    """
+    schema_url = "http://stsci.edu/schemas/roman_datamodel/reference_files/dark.schema"
+
+    def __init__(self, init=None, **kwargs):
+        super().__init__(init=init, **kwargs)
+
+        # Saved for DQ code implementation
+        # self.dq = dynamic_mask(self)
+        #
+        # # Implicitly create arrays
+        # self.dq = self.dq
+        # self.err = self.err
