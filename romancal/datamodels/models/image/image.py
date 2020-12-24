90fc84f2ea9eba05700bfb3b903b42c44a8cd059
PaulHuwe <phuwe@stsci.edu>
2020-12-24 03:09:54 -0500

    Tweaks from PR comments.

--- romancal/datamodels/models/image/image.py
+++ romancal/datamodels/models/image/image.py
@@ -4,28 +4,6 @@ class ImageModel(RomanDataModel):
     """
     A data model for 2D images.
 
-    Parameters
-    __________
-    data : numpy float32 array
-         The science data
-
-    dq : numpy uint32 array
-         Data quality array
-
-    err : numpy float32 array
-         Error array
-
-    zeroframe : numpy float32 array
-         Zeroframe array
-
-    var_poisson : numpy float32 array
-         variance due to poisson noise
-
-    var_rnoise : numpy float32 array
-         variance due to read noise
-
-    area : numpy float32 array
-         Pixel area map array
     """
     schema_url = "http://stsci.edu/schemas/roman_datamodel/image_files/image.schema"
 
