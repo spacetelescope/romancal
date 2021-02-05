8163faacb98efc089252be4f46937e6f4e7b2c0a
Ed Slavich <eslavich@stsci.edu>
2021-02-05 13:58:07 -0500

    Add entry point and test

--- romancal/stpipe/tests/test_integration.py
+++ romancal/stpipe/tests/test_integration.py
@@ -1,3 +1,4 @@
+from stpipe import entry_points
 from stpipe.utilities import import_class
 
 from romancal.stpipe.integration import get_steps
@@ -18,3 +19,10 @@ def test_get_steps():
         assert step_class.class_alias == class_alias
         if is_pipeline:
             assert issubclass(step_class, RomanPipeline)
+
+
+def test_entry_point():
+    all_steps = entry_points.get_steps()
+    roman_steps = [s for s in all_steps if s.package_name == "romancal"]
+    tuples = {(s.class_name, s.class_alias, s.is_pipeline) for s in roman_steps}
+    assert tuples == set(get_steps())
