02b0498c95acbac4e2723800a163bf1f512a879f
PaulHuwe <phuwe@stsci.edu>
2021-02-09 15:04:34 -0500

    Implemented ramp models & schemas. Moved group table from level1.schema to group.schema. Fixed JWST->Roman wording on core.schema.

---
+++ romancal/datamodels/gls_rampfit.py
@@ -0,0 +1,25 @@
+from .core import RomanDataModel
+
+class GLS_RampFitModel(RomanDataModel):
+    """
+    A data model for the optional output of the ramp fitting step
+    for the GLS algorithm.
+
+    Parameters
+    __________
+    yint : numpy float32 array
+         Y-intercept
+
+    sigyint : numpy float32 array
+         Sigma for Y-intercept
+
+    pedestal : numpy float32 array
+         Pedestal
+
+    crmag : numpy float32 array
+         CR magnitudes
+
+    sigcrmag : numpy float32 array
+         Sigma for CR magnitudes
+    """
+    schema_url = "http://stsci.edu/schemas/roman_datamodel/gls_rampfit.schema"
---
+++ romancal/datamodels/ramp.py
@@ -0,0 +1,41 @@
+from .core import RampDataModel
+#from ..lib.basic_utils import deprecate_class
+
+class RampModel(RomanDataModel):
+    """
+    A data model for 4D ramps.
+
+    Parameters
+    __________
+    data : numpy float32 array
+         The science data
+
+    pixeldq : numpy uint32 array
+         2-D data quality array for all planes
+
+    groupdq : numpy uint8 array
+         4-D data quality array for each plane
+
+    err : numpy float32 array
+         Error array
+
+    zeroframe : numpy float32 array
+         Zeroframe array
+
+    group : numpy table
+         group parameters table
+
+    int_times : numpy table
+         table of times for each integration
+
+    """
+    schema_url = "http://stsci.edu/schemas/roman_datamodel/ramp.schema"
+
+    def __init__(self, init=None, **kwargs):
+        super(RampModel, self).__init__(init=init, **kwargs)
+
+        # Implicitly create arrays
+        self.pixeldq = self.pixeldq
+        self.groupdq = self.groupdq
+        self.err = self.err
+
