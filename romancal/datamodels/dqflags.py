25a47dc5a141fc3fdb32deef42ab1cb5cc3c82b5
PaulHuwe <phuwe@stsci.edu>
2021-03-25 22:23:44 -0400

    Datamodels DQ modules updated to utilize STCAL versions.

--- romancal/datamodels/dqflags.py
+++ romancal/datamodels/dqflags.py
@@ -18,9 +18,8 @@ The data structure that stores bit flags is just the standard Python `int`,
 which provides 32 bits. Bits of an integer are most easily referred to using
 the formula `2**bit_number` where `bit_number` is the 0-index bit of interest.
 """
-from astropy.nddata.bitmask import interpret_bit_flags as ap_interpret_bit_flags
 
-from romancal.lib.basic_utils import multiple_replace
+from stcal import dqflags
 
 # Pixel-specific flags
 pixel = {'GOOD':             0,      # No bits set, all is good
@@ -57,7 +56,6 @@ pixel = {'GOOD':             0,      # No bits set, all is good
          'REFERENCE_PIXEL':  2**31,  # Pixel is a reference pixel
          }
 
-
 # Group-specific flags. Once groups are combined, these flags
 # are equivalent to the pixel-specific flags.
 group = {'GOOD':       pixel['GOOD'],
@@ -67,11 +65,10 @@ group = {'GOOD':       pixel['GOOD'],
          'DROPOUT':    pixel['DROPOUT'],
          }
 
-
-def interpret_bit_flags(bit_flags, flip_bits=None):
+def interpret_bit_flags(bit_flags, flip_bits=None, mnemonic_map=pixel):
     """Converts input bit flags to a single integer value (bit mask) or `None`.
 
-    Wraps `astropy.nddate.bitmask.interpret_bit_flags`, allowing the Roman
+    Wraps `astropy.nddate.bitmask.interpret_bit_flags`, allowing the
     bit mnemonics to be used in place of integers.
 
     Parameters
@@ -83,6 +80,10 @@ def interpret_bit_flags(bit_flags, flip_bits=None):
     flip_bits : bool, None
         See `astropy.nddata.bitmask.interpret_bit_flags`.
 
+    mnemonic_map : {str: int[,...]}
+        Dictionary associating the mnemonic string to an integer value
+        representing the set bit for that mnemonic.
+
     Returns
     -------
     bitmask : int or None
@@ -91,22 +92,8 @@ def interpret_bit_flags(bit_flags, flip_bits=None):
         If input string value was prepended with '~' (or ``flip_bits`` was set
         to `True`), then returned value will have its bits flipped
         (inverse mask).
-
-    Examples
-    --------
-    Using Roman mnemonics:
-    TBD
     """
-    bit_flags_dm = bit_flags
-    if isinstance(bit_flags, str):
-        dm_flags = {
-            key: str(val)
-            for key, val in pixel.items()
-        }
-        bit_flags_dm = multiple_replace(bit_flags, dm_flags)
-
-    return ap_interpret_bit_flags(bit_flags_dm, flip_bits=flip_bits)
-
+    return dqflags.interpret_bit_flags(bit_flags, flip_bits, mnemonic_map)
 
 def dqflags_to_mnemonics(dqflags, mnemonic_map=pixel):
     """Interpret value as bit flags and return the mnemonics
@@ -125,26 +112,5 @@ def dqflags_to_mnemonics(dqflags, mnemonic_map=pixel):
     mnemonics : {str[,...]}
         Set of mnemonics represented by the set bit flags
 
-    Examples
-    --------
-    >>> dqflags_to_mnemonics(1)
-    {'DO_NOT_USE'}
-
-    >>> dqflags_to_mnemonics(7)             #doctest: +SKIP
-    {'JUMP_DET', 'DO_NOT_USE', 'SATURATED'}
-
-    >>> dqflags_to_mnemonics(7) == {'JUMP_DET', 'DO_NOT_USE', 'SATURATED'}
-    True
-
-    >>> dqflags_to_mnemonics(1, mnemonic_map=pixel)
-    {'DO_NOT_USE'}
-
-    >>> dqflags_to_mnemonics(1, mnemonic_map=group)
-    {'DO_NOT_USE'}
     """
-    mnemonics = {
-        mnemonic
-        for mnemonic, value in mnemonic_map.items()
-        if (dqflags & value)
-    }
-    return mnemonics
+    return dqflags.dqflags_to_mnemonics(dqflags, mnemonic_map)
