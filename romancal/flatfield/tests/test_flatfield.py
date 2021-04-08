f26845cbee04560f6526b8b718b4f7b4a9b24f8c
perrygreenfield <perry@stsci.edu>
2021-04-08 16:01:51 -0400

    added changes to datamodels/tests/test_open.py

--- romancal/flatfield/tests/test_flatfield.py
+++ romancal/flatfield/tests/test_flatfield.py
@@ -1,7 +1,9 @@
 import pytest
 import numpy as np
 
-from romancal import datamodels
+from roman_datamodels import util, rconverters
+from roman_datamodels.datamodels import ImageModel, FlatRefModel
+from roman_datamodels.tests import util as testutil
 from romancal.flatfield import FlatFieldStep
 
 
@@ -17,29 +19,56 @@ def test_flatfield_step_interface(instrument, exptype):
 
     shape = (20, 20)
 
-    data = datamodels.ImageModel(shape)
-    data.meta.instrument.name = instrument
-    data.meta.exposure.type = exptype
-    data.meta.subarray.xstart = 1
-    data.meta.subarray.ystart = 1
-    data.meta.subarray.xsize = shape[1]
-    data.meta.subarray.ysize = shape[0]
 
-    flat = datamodels.FlatModel(shape)
-    flat.meta.instrument.name = instrument
-    flat.meta.subarray.xstart = 1
-    flat.meta.subarray.ystart = 1
-    flat.meta.subarray.xsize = shape[1]
-    flat.meta.subarray.ysize = shape[0]
-    flat.data += 1
-    flat.data[0,0] = np.nan
-    flat.err = np.random.random(shape) * 0.05
+    wfi_image = testutil.mk_level2_image(no_arrays=True)
+    wfi_image.meta.instrument.name = instrument
+    wfi_image.meta.instrument.detector = 'WFI01'
+    wfi_image.meta.instrument.optical_element = 'F158'
+    wfi_image.meta.exposure.type = exptype
+    wfi_image.data = np.ones(shape, dtype=np.float32)
+    wfi_image.dq = np.zeros(shape, dtype=np.uint32)
+    wfi_image.err = np.zeros(shape, dtype=np.float32)
+    wfi_image.var_poisson = np.zeros(shape, dtype=np.float32)
+    wfi_image.var_rnoise = np.zeros(shape, dtype=np.float32)
+    wfi_image.var_flat = np.zeros(shape, dtype=np.float32)
+    wfi_image.area = np.ones(shape, dtype=np.float32)
+    wfi_image_model = ImageModel(wfi_image)
+    #wfi_image_model.data[0, 0] = np.nan
+    # data = datamodels.ImageModel(shape)
+    # data.meta.instrument.name = instrument
+    # data.meta.exposure.type = exptype
+    # data.meta.subarray.xstart = 1
+    # data.meta.subarray.ystart = 1
+    # data.meta.subarray.xsize = shape[1]
+    # data.meta.subarray.ysize = shape[0]
+
+    flatref = rconverters.FlatRef()
+    meta = {}
+    testutil.add_ref_common(meta)
+    meta['instrument']['optical_element'] = 'F158'
+    meta['instrument']['dectector'] = 'WFI01'
+    meta['reftype'] = 'FLAT'
+    flatref['meta'] = meta
+    flatref['data'] = np.ones(shape, dtype=np.float32)
+    flatref['dq'] = np.zeros(shape, dtype=np.uint16)
+    #flatref.data[0, 0] = np.nan
+    flatref['err'] = (np.random.random(shape) * 0.05).astype(np.float32)
+    flatref_model = FlatRefModel(flatref)
+    # flat = datamodels.FlatModel(shape)
+    # flat.meta.instrument.name = instrument
+    # flat.meta.subarray.xstart = 1
+    # flat.meta.subarray.ystart = 1
+    # flat.meta.subarray.xsize = shape[1]
+    # flat.meta.subarray.ysize = shape[0]
+    # flat.data += 1
+    # flat.data[0,0] = np.nan
+    # flat.err = np.random.random(shape) * 0.05
 
     # override class attribute so only the `flat` type needs to be overriden
     # in the step call.
-    FlatFieldStep.reference_file_types = ["flat"]
-    result = FlatFieldStep.call(data, override_flat=flat)
+    # FlatFieldStep.reference_file_types = ["flat"]
+    result = FlatFieldStep.call(wfi_image_model, override_flat=flatref_model)
 
-    assert (result.data == data.data).all()
+    assert (result.data == wfi_image.data).all()
     assert result.var_flat.shape == shape
     assert result.meta.cal_step.flat_field == 'COMPLETE'
