0f23ef62466ac2e283e2a3fc32f55bee9fd4e214
perrygreenfield <perry@stsci.edu>
2021-04-14 13:34:45 -0400

    changes to support existing tests

--- romancal/datamodels/tests/test_models.py
+++ romancal/datamodels/tests/test_models.py
@@ -1,11 +1,21 @@
 import pytest
 
+from jsonschema import ValidationError
 import asdf
 import numpy as np
 from astropy.time import Time
 
-from romancal import datamodels
-from stdatamodels.validate import ValidationWarning
+from roman_datamodels import datamodels
+from roman_datamodels.tests import util
+from roman_datamodels.validate import ValidationWarning
+import roman_datamodels.rconverters as converters
+from roman_datamodels.extensions import DATAMODEL_EXTENSIONS
+from roman_datamodels.util import get_schema_uri_from_converter
+from rad.integration import get_resource_mappings
+
+EXPECTED_COMMON_REFERENCE = \
+    {'$ref': 'http://stsci.edu/schemas/datamodels/roman/reference_files/ref_common-1.0.0'}
+
 
 # Helper class to return a set of model subclasses
 def get_subclasses(model_class, include_base_model=True):
@@ -21,133 +31,107 @@ def get_subclasses(model_class, include_base_model=True):
 
     return set(class_list)
 
-def test_model_schemas(tmp_path):
-    for model_class in get_subclasses(datamodels.RomanDataModel):
-        asdf.schema.load_schema(model_class.schema_url)
+
+def test_model_schemas():
+    dmodels = datamodels.model_registry.keys()
+    for model in dmodels:
+        schema_uri = next(t for t in DATAMODEL_EXTENSIONS[0].tags
+                          if t._tag_uri == model._tag)._schema_uri
+        asdf.schema.load_schema(schema_uri)
+
 
 # Testing core schema
 def test_core_schema(tmp_path):
     # Set temporary asdf file
     file_path = tmp_path / "test.asdf"
-
-    # Define meta dictionary of required key / value pairs
-    meta = {"meta":
-                {"instrument": {
-                     "detector": "WFI01",
-                     "name": "WFI"
-                 },
-                 "telescope": "ROMAN",
-                 "model_type": "RomanDataModel",
-                 "date": Time('2021-02-20T02:20:00.123456789', format='isot', scale='utc')
-                }
-            }
-
-    # Testing bad core asdf file
-    with asdf.AsdfFile(meta) as af:
-        # Test for invalid entry
-        af["meta"]["telescope"] = "NOTROMAN"
+    wfi_image = util.mk_level2_image(arrays=(10, 10))
+    with asdf.AsdfFile() as af:
+        af.tree = {'roman': wfi_image}
+        with pytest.raises(ValidationError):
+            af.tree['roman'].meta.telescope = 'NOTROMAN'
+        af.tree['roman'].meta['telescope'] = 'NOTROMAN'
+        with pytest.raises(ValidationError):
+            af.write_to(file_path)
+        af.tree['roman'].meta.telescope = 'ROMAN'
         af.write_to(file_path)
-
+    # Now mangle the file
+    with open(file_path, 'rb') as fp:
+        fcontents = fp.read()
+    romanloc = fcontents.find(bytes('ROMAN', 'utf-8'))
+    newcontents = fcontents[:romanloc] + \
+        bytes('X', 'utf-8') + fcontents[romanloc + 1:]
+    with open(file_path, 'wb') as fp:
+        fp.write(newcontents)
+    with pytest.raises(ValidationError):
         with datamodels.open(file_path) as model:
-            with pytest.warns(ValidationWarning):
-                model.validate()
-            assert model["meta"]["telescope"] == "NOTROMAN"
+            pass
+    asdf.get_config().validate_on_read = False
+    with datamodels.open(file_path) as model:
+        assert model.meta.telescope == 'XOMAN'
+    asdf.get_config().validate_on_read = True
 
-# Helper function to confirm that a reference exists within a schema
-def assert_referenced_schema(schema_uri, ref_uri):
-    # Hide function from pytest traceback
-    __tracebackhide__ = True
-
-    # Load schema
-    schema = asdf.schema.load_schema(schema_uri)
-
-    # Search for reference in schema
-    for subschema in schema.get("allOf", []):
-        if asdf.generic_io.resolve_uri(schema_uri, subschema.get("$ref")) == ref_uri:
-            # Reference found
-            return
-    # Reference not found
-    assert False, f"Schema '{schema_uri}' does not reference '{ref_uri}' in a top-level allOf " \
-                  f"combiner"
-
-
-# Define base meta dictionary of required key / value pairs for reference files
-REFERENCEFILE_SCHEMA_DICT = {
-    "meta": {
-                    "author": "Space Telescope Science Institute",
-                    "description": "Test file",
-                    "pedigree": "Test",
-                    "reftype": "BASE",
-                    "useafter": Time('2021-02-20T02:20:00.123456789',format='isot', scale='utc'),
-                    # Key/value pairs from core.schema
-                    "date": Time('2021-02-20T02:20:00.123456789',format='isot', scale='utc'),
-                    "instrument": {
-                        "detector": "WFI01",
-                        "name": "WFI"
-                    },
-                    "telescope": "ROMAN"
-        }
-    }
 
 # Testing all reference file schemas
 def test_reference_file_model_base(tmp_path):
     # Set temporary asdf file
     file_path = tmp_path / "test.asdf"
 
-    # Get reference dictionary base
-    meta = REFERENCEFILE_SCHEMA_DICT
-
-    # Testing referencefile asdf file
-    with asdf.AsdfFile(meta) as af:
-        af.write_to(file_path)
-
-        # Ensure that base referencefile schema contains core schema
-        assert_referenced_schema(datamodels.reference_files.referencefile.ReferenceFileModel.schema_url,
-                                 datamodels.core.RomanDataModel.schema_url)
-
-        # Test that asdf file opens properly
-        assert datamodels.open(file_path)
-
-        # Confirm that asdf file is opened as base referencefile model
-        with datamodels.open(file_path) as model:
-            assert isinstance(model, datamodels.reference_files.referencefile.ReferenceFileModel)
+    # Get all reference file classes
+    tags = [t for t in DATAMODEL_EXTENSIONS[0].tags if "/reference_files/" in t._tag_uri]
+    schemas = []
+    for tag in tags:
+        schema = asdf.schema.load_schema(tag._schema_uri)
+        # Check that schema references common reference schema
+        allofs = schema['properties']['meta']['allOf']
+        found_common = False
+        for item in allofs:
+            if item == EXPECTED_COMMON_REFERENCE:
+                found_common = True
+        if not found_common:
+            raise ValueError("Reference schema does not include ref_common")
 
 
-# Common tests for all ReferenceFileModel subclasses
-@pytest.mark.parametrize("model_class", sorted(get_subclasses(
-    datamodels.reference_files.referencefile.ReferenceFileModel, include_base_model=False),
-    key=lambda x: x.__name__))
-def test_reference_file_model(tmp_path, model_class):
-    # Ensure that specific reference file schema contains the base module schema
-    assert_referenced_schema(model_class.schema_url,
-                             datamodels.reference_files.referencefile.ReferenceFileModel.schema_url)
+def test_opening_flat_ref(tmp_path):
+    # First make test reference file
+    file_path = tmp_path / 'testflat.asdf'
+    util.mk_flat(file_path)
+    flat = datamodels.open(file_path)
+    assert flat.meta.instrument.optical_element == 'F158'
+    assert isinstance(flat, datamodels.FlatRefModel)
+
 
 # Common tests for nonReference Models
 NON_REFERENCE_MODELS = get_subclasses(datamodels.RomanDataModel, include_base_model=False) - \
-                       get_subclasses(datamodels.reference_files.referencefile.ReferenceFileModel,
-                                      include_base_model=True)
+    get_subclasses(datamodels.reference_files.referencefile.ReferenceFileModel,
+                   include_base_model=True)
+
+
 @pytest.mark.parametrize("model_class", sorted(NON_REFERENCE_MODELS, key=lambda x: x.__name__))
 def test_non_reference_file_model(tmp_path, model_class):
     # Ensure that nonReference files contain core schema
     assert_referenced_schema(model_class.schema_url,
                              datamodels.core.RomanDataModel.schema_url)
 
+
 # FlatModel tests
 def test_flat_model(tmp_path):
     # Set temporary asdf file
     file_path = tmp_path / "test.asdf"
 
-    # Get reference dictionary base
-    meta = REFERENCEFILE_SCHEMA_DICT
+    meta = {}
+    util.add_ref_common(meta)
+    meta['reftype'] = "FLAT"
+    flatref = converters.FlatRef()
+    flatref['meta'] = meta
+    flatref.meta.instrument['optical_element'] = 'F062'
+    shape = (4096, 4096)
+    flatref['data'] = np.zeros(shape, dtype=np.float32)
+    flatref['dq'] = np.zeros(shape, dtype=np.uint32)
+    flatref['err'] = np.zeros(shape, dtype=np.float32)
 
     # Testing flat file asdf file
     with asdf.AsdfFile(meta) as af:
-        # Add required flat file elements
-        af["meta"]["reftype"] = "FLAT"
-        af["meta"]["model_type"] = "FlatModel"
-        af["data"] = np.zeros((4096, 4096))
-        af["dq"] = np.zeros((4096, 4096))
-        af["derr"] = np.zeros((4096, 4096))
+        af.tree = {'roman': flatref}
         af.write_to(file_path)
 
         # Test that asdf file opens properly
@@ -156,20 +140,4 @@ def test_flat_model(tmp_path):
                 model.validate()
 
             # Confirm that asdf file is opened as flat file model
-            assert isinstance(model, datamodels.reference_files.flat.FlatModel)
-
-# Test date object
-def test_meta_date_management(tmp_path):
-    model = datamodels.RomanDataModel({
-        "meta": {
-            "date": Time("2000-01-01T00:00:00.000"),
-            "instrument": {"name": "WFI", "detector": "WFI01", "optical_element": "F062"},
-            "telescope": "ROMAN",
-        }
-    })
-    assert model.meta.date == Time("2000-01-01T00:00:00.000")
-    model.save(str(tmp_path/"test.asdf"))
-    assert abs((Time.now() - model.meta.date).value) < 1.0
-
-    model = datamodels.RomanDataModel()
-    assert abs((Time.now() - model.meta.date).value) < 1.0
+            assert isinstance(model, datamodels.FlatRefModel)
