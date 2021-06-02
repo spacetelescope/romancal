9c3d55c5220a2420694f2d7be0852d0b3b265eda
Nadia Dencheva <nadia.astropy@gmail.com>
2021-06-02 10:08:34 -0400

    add a test for retrieving a spectral file from CRDS (#225)

--- romancal/stpipe/tests/test_core.py
+++ romancal/stpipe/tests/test_core.py
@@ -51,3 +51,28 @@ def test_get_reference_file(step_class):
 
     with step.open_model(reference_path) as reference_model:
         assert isinstance(reference_model, FlatRefModel)
+
+
+@pytest.mark.skipif(
+    os.environ.get("CI") == "true",
+    reason="Roman CRDS servers are not currently available outside the internal network"
+)
+@pytest.mark.parametrize("step_class", [RomanPipeline, RomanStep])
+def test_get_reference_file_spectral(step_class):
+    """
+    Test that CRDS is properly integrated.
+    """
+    im = mk_level2_image(arrays=(20, 20))
+    # This will be brittle while we're using the dev server.
+    # If this test starts failing mysteriously, check the
+    # metadata values against the flat rmap.
+    im.meta.instrument.optical_element = "GRISM"
+    im.meta.observation.start_time = Time('2021-01-01T12:00:00')
+    model = ImageModel(im)
+
+    step = step_class()
+    reference_path = step.get_reference_file(model, "flat")
+
+    with step.open_model(reference_path) as reference_model:
+        assert isinstance(reference_model, FlatRefModel)
+        assert reference_model.meta.instrument.optical_element == "GRISM"
