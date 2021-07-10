49a6b231cba1d94d59be974a6024b425dddca00b
PaulHuwe <phuwe@stsci.edu>
2021-07-10 16:40:20 -0400

    Updated DQ support and tests to use RDM.

--- romancal/dq_init/dq_init_step.py
+++ romancal/dq_init/dq_init_step.py
@@ -1,12 +1,11 @@
 #! /usr/bin/env python
 
 from romancal.stpipe import RomanStep
-#from stcal.dq_init.dq_init_step import DQInitStep
-from stcal.dq_init import dq_init_step
+import roman_datamodels as rdm
 
 __all__ = ["DQInitStep"]
 
-class DQInitStep(RomanStep,dq_init_step.DQInitStep):
+class DQInitStep(RomanStep):
     """Initialize the Data Quality extension from the
     mask reference file.
 
@@ -16,49 +15,68 @@ class DQInitStep(RomanStep,dq_init_step.DQInitStep):
     instead.  The dq attribute of the MASK model is bitwise OR'd
     with the pixeldq (or dq) attribute of the input model.
     """
-    pass
 
+    reference_file_types = ['mask']
 
+    def process(self, input):
+        """Perform the dq_init calibration step
 
+        Parameters
+        ----------
+        input : science datamodel
+            input science datamodel
 
-# class DQInitStep(dq_init_step.DQInitStep):
-#     """Initialize the Data Quality extension from the
-#     mask reference file.
-#
-#     The dq_init step initializes the pixeldq attribute of the
-#     input datamodel using the MASK reference file.  For some
-#     Guiding exp_types, initalize the dq attribute of the input model
-#     instead.  The dq attribute of the MASK model is bitwise OR'd
-#     with the pixeldq (or dq) attribute of the input model.
-#     """
-#
-#     def __init__(self, RomanStep):
-#         self.__init_subclass__()class__ = RomanStep
-#
-#     pass
+        Returns
+        -------
+        output_model : science datamodel
+            result science datamodel
+        """
 
-# class DQInitStep(RomanStep):
-#     """Initialize the Data Quality extension from the
-#     mask reference file.
-#
-#     The dq_init step initializes the pixeldq attribute of the
-#     input datamodel using the MASK reference file.  For some
-#     Guiding exp_types, initalize the dq attribute of the input model
-#     instead.  The dq attribute of the MASK model is bitwise OR'd
-#     with the pixeldq (or dq) attribute of the input model.
-#     """
-#
-#     def process(self):
-#         """Perform the dq_init calibration step
-#
-#         Parameters
-#         ----------
-#         input : science datamodel
-#             input science datamodel
-#
-#         Returns
-#         -------
-#         output_model : science datamodel
-#             result science datamodel
-#         """
-#         return RomanStep(dq_init_step.DQInitStep())
+        # Try to open the input as a regular RampModel
+        try:
+            input_model = self.open_model(input)
+
+        # Commented Guider Code
+        #
+        #     # Check to see if it's Guider raw data
+        #     if input_model.meta.exposure.type in dq_initialization.guider_list:
+        #         # Reopen as a GuiderRawModel
+        #         input_model.close()
+        #         input_model = rdm.GuiderRawModel(input)
+        #         self.log.info("Input opened as GuiderRawModel")
+        #
+        # except (TypeError, ValueError):
+        #     # If the initial open attempt fails,
+        #     # try to open as a GuiderRawModel
+        #     try:
+        #         input_model = rdm.GuiderRawModel(input)
+        #         self.log.info("Input opened as GuiderRawModel")
+        #     except (TypeError, ValueError):
+        #         self.log.error("Unexpected or unknown input model type")
+        except Exception:
+            self.log.error("Can't open input")
+            raise
+
+        # Retreive the mask reference file name
+        self.mask_filename = self.get_reference_file(input_model, 'mask')
+        self.log.info('Using MASK reference file %s', self.mask_filename)
+
+        # Check for a valid reference file
+        if self.mask_filename == 'N/A':
+            self.log.warning('No MASK reference file found')
+            self.log.warning('DQ initialization step will be skipped')
+            result = input_model.copy()
+            result.meta.cal_step.dq_init = 'SKIPPED'
+            return result
+
+        # Load the reference file
+        mask_model = rdm.MaskModel(self.mask_filename)
+
+        # Apply the step
+        result = dq_initialization.correct_model(input_model, mask_model)
+
+        # Close the data models for the input and ref file
+        input_model.close()
+        mask_model.close()
+
+        return result
