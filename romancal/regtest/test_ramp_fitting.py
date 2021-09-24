7a730542e1220f29ba2fb28674929064014ba33e
Nadia Dencheva <nadia.dencheva@gmail.com>
2021-09-24 12:45:13 +0300

    add a test with optional output

--- romancal/regtest/test_ramp_fitting.py
+++ romancal/regtest/test_ramp_fitting.py
@@ -8,9 +8,14 @@ def test_ramp_fitting_step(rtdata, ignore_asdf_paths):
     rtdata.get_data("WFI/image/ramp.asdf")
     rtdata.input = "ramp.asdf"
 
+    args = ["romancal.step.RampFitStep", rtdata.input, '--save_opt=True', '--opt_name=rampfit_opt.asdf']
+    RomanStep.from_cmdline(args)
     output = "ramp_0_rampfitstep.asdf"
     rtdata.output = output
-    args = ["romancal.step.RampFitStep", rtdata.input]
-    RomanStep.from_cmdline(args)
+    rtdata.get_truth(f"truth/WFI/image/{output}")
+    assert (compare_asdf(rtdata.output, rtdata.truth, **ignore_asdf_paths) is None)
+
+    output = "rampfit_opt_fitopt.asdf"
+    rtdata.output = output
     rtdata.get_truth(f"truth/WFI/image/{output}")
     assert (compare_asdf(rtdata.output, rtdata.truth, **ignore_asdf_paths) is None)
