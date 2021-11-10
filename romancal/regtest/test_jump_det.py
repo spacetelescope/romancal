f417b84bd594a944b4775f3b350074ecf3d846b8
dmggh <grumm@stsci.edu>
2021-11-10 10:29:07 -0500

    Updated names of input and truth files

--- romancal/regtest/test_jump_det.py
+++ romancal/regtest/test_jump_det.py
@@ -10,11 +10,11 @@ def test_jump_detection_step(rtdata, ignore_asdf_paths):
     """ Function to run and compare Jump Detection files. Note: This should
         include tests for overrides etc. """
     rtdata.get_data("WFI/image/l1_0006_science_raw_dqinitstep.asdf")
-    rtdata.input = "l1_0006_science_raw_dqinitstep.asdf"
+    rtdata.input = "l1_0007_science_raw_dqinitstep.asdf"
 
     args = ["romancal.step.JumpStep", rtdata.input]
     RomanStep.from_cmdline(args)
-    output = "l1_0006_science_raw_dqinitstep_jumpstep.asdf"
+    output = "l1_0007_science_raw_dqinitstep_jumpstep.asdf"
     rtdata.output = output
     rtdata.get_truth(f"truth/WFI/image/{output}")
     assert (compare_asdf(rtdata.output, rtdata.truth,
