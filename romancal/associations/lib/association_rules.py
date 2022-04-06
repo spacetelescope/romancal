707118c28dc8842f80231be4d52273f62192c2a9
Dave Davis <ddavis@stsci.edu>
2022-04-06 15:27:58 -0400

    rcal-317 commit changes

--- romancal/associations/lib/association_rules.py
+++ romancal/associations/lib/association_rules.py
@@ -4,9 +4,8 @@ Notes
 -----
 These associations are specifically defined for use in DMS.
 """
-from romancal.associations import Association
 from romancal.associations.registry import RegistryMarker
 
 from romancal.associations.lib import (rules_elpp_base)
-#RegistryMarker.mark(rules_level2b)
-RegistryMarker.mark(rules_elpp)
+
+RegistryMarker.mark(rules_elpp_base)
