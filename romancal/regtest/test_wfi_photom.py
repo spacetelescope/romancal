b11d01d91c8aaf85d635f932bf847a419ba5e25c
PaulHuwe <phuwe@stsci.edu>
2022-04-20 13:59:38 -0400

    Added SOC test for absolute photometric calibration. Tweak logging in photom step.

--- romancal/regtest/test_wfi_photom.py
+++ romancal/regtest/test_wfi_photom.py
@@ -1,54 +1,103 @@
-import os
 import pytest
 
 import roman_datamodels as rdm
 from romancal.stpipe import RomanStep
 from romancal.step import PhotomStep
 from .regtestdata import compare_asdf
+from astropy import units as u
+import math
 
 
 @pytest.mark.bigdata
 def test_absolute_photometric_calibration(rtdata, ignore_asdf_paths):
-    # DMS25 Test: Testing retrieval of best ref file for image data,
-    # and creation of a ramp file with CRDS selected mask file applied.
+    # DMS140 Test: Testing application of photometric correction using CRDS selected photom file.
 
-    rtdata.get_data("WFI/image/r0000101001001001001_01101_0001_WFI01_uncal.asdf")
-    rtdata.input = "r0000101001001001001_01101_0001_WFI01_uncal.asdf"
+    rtdata.get_data("WFI/image/r0000201001001001001_01101_0001_WFI01_cal.asdf")
+    rtdata.input = "r0000201001001001001_01101_0001_WFI01_cal.asdf"
 
-    # Test CRDS
+    # Define step (for running and log access)
     step = PhotomStep()
-    model = rdm.open(rtdata.input)
 
     #  In Wide Field Imaging mode, the DMS shall generate Level 2 science data products with
     #  absolute photometry calibrated in the WFI filter used for the exposure.
-    step.log.info('DMS140 MSG: Testing absolute photometry calibrated image data, '
+    step.log.info('DMS140 MSG: Testing absolute photometry calibrated image data. '
                   'Success is creation of a Level 2 image file with CRDS selected '
                   'photom file applied.')
 
-
-
     step.log.info(f'DMS140 MSG: Image data file: {rtdata.input.rsplit("/", 1)[1]}')
-    # ref_file_path = step.get_reference_file(model, "photom")
-    # step.log.info(f'DMS140 MSG: CRDS matched photom file: {ref_file_path.rsplit("/", 1)[1]}')
-    # ref_file_name = os.path.split(ref_file_path)[-1]
-    #
-    # assert "roman_wfi_photom" in ref_file_name
 
-    # Test DQInitStep
-    output = "r0000101001001001001_01101_0001_WFI01_dqinitstep.asdf"
+    # Note: if any of the following tests fail, check for a different photom match from CRDS.
+    # Values come from roman_wfi_photom_0034.asdf
+
+    # Test PhotomStep
+    output = "r0000201001001001001_01101_0001_WFI01_cal_photomstep.asdf"
     rtdata.output = output
-    args = ["romancal.step.DQInitStep", rtdata.input]
-    step.log.info('DMS140 MSG: Running data quality initialization step.'
+    args = ["romancal.step.PhotomStep", rtdata.input]
+    step.log.info('DMS140 MSG: Running photometric conversion step.'
                   ' The first ERROR is expected, due to extra CRDS parameters'
                   ' not having been implemented yet.')
     RomanStep.from_cmdline(args)
-    ramp_out = rdm.open(rtdata.output)
-    step.log.info(f'DMS140 MSG: Does ramp data contain pixeldq from mask file? : '
-                  f'{("roman.pixeldq" in ramp_out.to_flat_dict())}')
-    assert "roman.pixeldq" in ramp_out.to_flat_dict()
+    photom_out = rdm.open(rtdata.output)
+
+    step.log.info(f'DMS140 MSG: Photom step recorded as complete? : '
+                  f'{photom_out.meta.cal_step.photom == "COMPLETE"}')
+    assert photom_out.meta.cal_step.photom == "COMPLETE"
+
+    step.log.info('DMS140 MSG: Photom megajansky conversion calculated? : ' +
+                  str((photom_out.meta.photometry.conversion_megajanskys.unit == u.MJy / u.sr) and
+                       math.isclose(photom_out.meta.photometry.conversion_megajanskys.value,
+                                    0.3214, abs_tol=0.0001)))
+    assert photom_out.meta.photometry.conversion_megajanskys.unit == u.MJy / u.sr
+    assert math.isclose(photom_out.meta.photometry.conversion_megajanskys.value,
+                        0.3214, abs_tol=0.0001)
+
+    step.log.info('DMS140 MSG: Photom microjanskys conversion calculated? : ' +
+                  str((photom_out.meta.photometry.conversion_microjanskys.unit ==
+                       u.uJy / u.arcsec ** 2) and
+                      (math.isclose(photom_out.meta.photometry.conversion_microjanskys.value,
+                                    7.5549, abs_tol=0.0001))))
+    assert photom_out.meta.photometry.conversion_microjanskys.unit == u.uJy / u.arcsec ** 2
+    assert math.isclose(photom_out.meta.photometry.conversion_microjanskys.value,
+                        7.5549, abs_tol=0.0001)
+
+    step.log.info('DMS140 MSG: Pixel area in steradians calculated? : ' +
+                  str((photom_out.meta.photometry.pixelarea_steradians.unit == u.sr) and
+                      (math.isclose(photom_out.meta.photometry.pixelarea_steradians.value,
+                                    2.8083e-13, abs_tol=1.0e-17))))
+    assert photom_out.meta.photometry.pixelarea_steradians.unit == u.sr
+    assert math.isclose(photom_out.meta.photometry.pixelarea_steradians.value, 2.8083e-13,
+                        abs_tol=1.0e-17)
+
+    step.log.info('DMS140 MSG: Pixel area in square arcseconds calculated? : ' +
+                  str((photom_out.meta.photometry.pixelarea_arcsecsq.unit == u.arcsec ** 2) and
+                      (math.isclose(photom_out.meta.photometry.pixelarea_arcsecsq.value,
+                                    0.011948, abs_tol=1.0e-6))))
+    assert photom_out.meta.photometry.pixelarea_arcsecsq.unit == u.arcsec ** 2
+    assert math.isclose(photom_out.meta.photometry.pixelarea_arcsecsq.value, 0.011948,
+                        abs_tol=1.0e-6)
+
+    step.log.info('DMS140 MSG: Photom megajansky conversion uncertainty calculated? : ' +
+                  str((photom_out.meta.photometry.conversion_megajanskys_uncertainty.unit
+                       == u.MJy / u.sr) and
+                      (math.isclose(photom_out.meta.photometry.\
+                                    conversion_megajanskys_uncertainty.value, 0.0,
+                                    abs_tol=1.0e-6))))
+    assert photom_out.meta.photometry.conversion_megajanskys_uncertainty.unit == u.MJy / u.sr
+    assert math.isclose(photom_out.meta.photometry.conversion_megajanskys_uncertainty.value, 0.0,
+                        abs_tol=1.0e-6)
+
+    step.log.info('DMS140 MSG: Photom megajansky conversion uncertainty calculated? : ' +
+                  str((photom_out.meta.photometry.conversion_microjanskys_uncertainty.unit ==
+                       u.uJy / u.arcsec ** 2) and
+                      (math.isclose(photom_out.meta.photometry.\
+                                    conversion_microjanskys_uncertainty.value, 0.0,
+                                    abs_tol=1.0e-6))))
+    assert photom_out.meta.photometry.conversion_microjanskys_uncertainty.unit ==  \
+           u.uJy / u.arcsec ** 2
+    assert math.isclose(photom_out.meta.photometry.conversion_microjanskys_uncertainty.value, 0.0,
+                        abs_tol=1.0e-6)
 
     rtdata.get_truth(f"truth/WFI/image/{output}")
-    step.log.info(f'DMS140 MSG: Was the proper data quality array initialized'
-                  ' for the ramp data produced? : '
-                  f'{(compare_asdf(rtdata.output, rtdata.truth, **ignore_asdf_paths) is None)}')
+    step.log.info(f'DMS140 MSG: Was the proper absolute photometry calibrated image data produced?'
+                  f' : {(compare_asdf(rtdata.output, rtdata.truth, **ignore_asdf_paths) is None)}')
     assert compare_asdf(rtdata.output, rtdata.truth, **ignore_asdf_paths) is None
