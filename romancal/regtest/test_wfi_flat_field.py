44245e90775ec8855da21caed5ff91de152737b6
Dave Davis <ddavis@stsci.edu>
2022-05-17 10:59:25 -0400

    RCAL-340 Updates to flat field code to handle no flat for grism data & remove skip

--- romancal/regtest/test_wfi_flat_field.py
+++ romancal/regtest/test_wfi_flat_field.py
@@ -31,13 +31,13 @@ def test_flat_field_image_step(rtdata, ignore_asdf_paths):
                         **ignore_asdf_paths) is None
 
 
-@pytest.mark.skip(reason="There are no grism flats.")
 @pytest.mark.bigdata
 def test_flat_field_grism_step(rtdata, ignore_asdf_paths):
     """Test for the flat field step using grism data."""
 
-    rtdata.get_data("WFI/grism/l2_0005_grism_rate.asdf")
-    rtdata.input = "l2_0005_grism_rate.asdf"
+    input_file = "r0000101001001001001_01102_0001_WFI01_uncal.asdf"
+    rtdata.get_data(f"WFI/grism/{input_file}")
+    rtdata.input = input_file
 
     # Test CRDS
     step = FlatFieldStep()
@@ -47,7 +47,7 @@ def test_flat_field_grism_step(rtdata, ignore_asdf_paths):
     assert "roman_wfi_flat" in ref_file_name
 
     # Test FlatFieldStep
-    output = "l2_0005_grism_rate_flat.asdf"
+    output = "r0000201001001001002_01101_0001_WFI01_flat.asdf"
     rtdata.output = output
     args = ["romancal.step.FlatFieldStep", rtdata.input]
     RomanStep.from_cmdline(args)
