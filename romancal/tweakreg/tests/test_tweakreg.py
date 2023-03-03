f11b47d7273bdd836b2af338dff5dd82d659dd52
M. Teodoro <mteodoro@stsci.edu>
2023-03-03 11:37:19 -0500

    Add description to unit tests.

--- romancal/tweakreg/tests/test_tweakreg.py
+++ romancal/tweakreg/tests/test_tweakreg.py
@@ -1,6 +1,6 @@
 from roman_datamodels import datamodels as rdm
 from romancal.tweakreg.tweakreg_step import TweakRegStep
-from roman_datamodels.testing import utils as testutil
+from roman_datamodels import maker_utils
 import os
 import csv
 import asdf
@@ -8,10 +8,14 @@ import pytest
 
 
 def load_base_image_wcs(input_dm):
-    # data from romanisim simulated image 'l1-270-66-gaia-2016-sca7_cal.asdf'
-    # command used to create simulated image:
-    # romanisim-make-image --catalog ./rsim_cat_F158.ecsv --radec 270.0 66.0 --bandpass F158 --sca 7
-    # --usecrds --webbpsf --date 2026 1 1 --level 1 l1-270-66-gaia-2016-sca7.asdf
+    # create a base WCS using data from a romanisim simulated image
+    # Note: this is the command used to create the simulated image:
+    #   romanisim-make-image \
+    #     --catalog ./rsim_cat_F158.ecsv \
+    #     --radec 270.0 66.0 \
+    #     --bandpass F158 --sca 7 \
+    #     --usecrds --webbpsf --date 2026 1 1 \
+    #     --level 1 l1-270-66-gaia-2016-sca7.asdf
 
     # update meta.wcsinfo
     input_dm.meta.wcsinfo.v2_ref = 0.42955128282521254
@@ -21,14 +25,13 @@ def load_base_image_wcs(input_dm):
     input_dm.meta.wcsinfo.dec_ref = 66.0
     input_dm.meta.wcsinfo.roll_ref = 60
 
-    # read saved WCS object
+    # read saved WCS object and add it to meta
     full_path_to_wcs_file = os.path.join(
         os.path.dirname(__file__), "data/base_image_wcs.asdf"
     )
     asdf_file = asdf.open(full_path_to_wcs_file)
     wcs = asdf_file.tree["wcs"]
-
-    return wcs
+    input_dm.meta["wcs"] = wcs
 
 
 def create_base_image_source_catalog(tmpdir, output_filename):
@@ -56,14 +59,15 @@ def add_tweakreg_catalog_attribute(tmpdir, input_dm):
 
 @pytest.fixture
 def base_image(tmpdir):
-    l2 = testutil.mk_level2_image(shape=(4088, 4088))
+    l2 = maker_utils.mk_level2_image(shape=(4088, 4088))
     # update wcsinfo and add WCS from a simulated image
-    l2.meta["wcs"] = load_base_image_wcs(l2)
+    load_base_image_wcs(l2)
     l2im = rdm.ImageModel(l2)
     return l2im
 
 
 def test_tweakreg_raises_attributeerror_on_missing_tweakreg_catalog(base_image):
+    """Test that TweakReg raises an AttributeError if meta.tweakreg_catalog is missing."""
     img = base_image
     with pytest.raises(Exception) as exec_info:
         TweakRegStep.call([img])
@@ -72,6 +76,7 @@ def test_tweakreg_raises_attributeerror_on_missing_tweakreg_catalog(base_image):
 
 
 def test_tweakreg_returns_modelcontainer(tmpdir, base_image):
+    """Test that TweakReg returns a ModelContainer."""
     img = base_image
     add_tweakreg_catalog_attribute(tmpdir, img)
     res = TweakRegStep.call([img])
@@ -79,7 +84,8 @@ def test_tweakreg_returns_modelcontainer(tmpdir, base_image):
     assert type(res) == rdm.ModelContainer
 
 
-def test_tweakreg_updates_cal_step(tmpdir):
+def test_tweakreg_updates_cal_step(tmpdir, base_image):
+    """Test that TweakReg updates meta.cal_step with tweakreg = COMPLETE."""
     img = base_image
     add_tweakreg_catalog_attribute(tmpdir, img)
     res = TweakRegStep.call([img])
@@ -88,7 +94,8 @@ def test_tweakreg_updates_cal_step(tmpdir):
     assert res[0].meta.cal_step.tweakreg == "COMPLETE"
 
 
-def test_tweakreg_creates_group(tmpdir):
+def test_tweakreg_updates_group_id(tmpdir, base_image):
+    """Test that TweakReg updates 'group_id' with a non-zero length string."""
     img = base_image
     add_tweakreg_catalog_attribute(tmpdir, img)
     res = TweakRegStep.call([img])
