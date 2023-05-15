ab4f357ca4b131c35d00becb159a5ea43f2676a5
M. Teodoro <mteodoro@stsci.edu>
2023-05-15 17:15:54 -0400

    Error-handling improvements to TweakReg and get_catalog method.

--- romancal/tweakreg/tests/test_tweakreg.py
+++ romancal/tweakreg/tests/test_tweakreg.py
@@ -3,7 +3,6 @@ import json
 import os
 from io import StringIO
 from typing import Tuple
-import json
 
 import numpy as np
 import pytest
@@ -30,45 +29,6 @@ from romancal.tweakreg.tweakreg_step import (
 class MockConnectionError:
     def __init__(self, *args, **kwargs):
         raise requests.exceptions.ConnectionError
-def create_asn_file(tmp_path):
-    asn_content = """
-        {
-            "asn_type": "None",
-            "asn_rule": "DMS_ELPP_Base",
-            "version_id": null,
-            "code_version": "0.9.1.dev28+ge987cc9.d20230106",
-            "degraded_status": "No known degraded exposures in association.",
-            "program": "noprogram",
-            "constraints": "No constraints",
-            "asn_id": "a3001",
-            "target": "none",
-            "asn_pool": "test_pool_name",
-            "products": [
-                {
-                    "name": "files.asdf",
-                    "members": [
-                        {
-                            "expname": "img_1.asdf",
-                            "exptype": "science"
-                        },
-                        {
-                            "expname": "img_2.asdf",
-                            "exptype": "science"
-                        }
-                    ]
-                }
-            ]
-        }
-"""
-    asn_file_path = str(tmp_path / "sample_asn.json")
-    asn_file = StringIO()
-    asn_file.write(asn_content)
-    with open(asn_file_path, mode="w") as f:
-        print(asn_file.getvalue(), file=f)
-
-    return asn_file_path
-
-
 def create_asn_file(tmp_path):
     asn_content = """
         {
@@ -294,9 +254,7 @@ def create_wcs_for_tweakreg_pipeline(input_dm, shift_1=0, shift_2=0):
     tel2sky = _create_tel2sky_model(input_dm)
 
     # create required frames
-    detector = cf.Frame2D(
-        name="detector", axes_order=(0, 1), unit=(u.pix, u.pix)
-    )
+    detector = cf.Frame2D(name="detector", axes_order=(0, 1), unit=(u.pix, u.pix))
     v2v3 = cf.Frame2D(
         name="v2v3",
         axes_order=(0, 1),
@@ -319,14 +277,9 @@ def create_wcs_for_tweakreg_pipeline(input_dm, shift_1=0, shift_2=0):
 
 def get_catalog_data(input_dm):
     gaia_cat = get_catalog(ra=270, dec=66, sr=100 / 3600)
-    gaia_source_coords = [
-        (ra, dec) for ra, dec in zip(gaia_cat["ra"], gaia_cat["dec"])
-    ]
+    gaia_source_coords = [(ra, dec) for ra, dec in zip(gaia_cat["ra"], gaia_cat["dec"])]
     catalog_data = np.array(
-        [
-            input_dm.meta.wcs.world_to_pixel(ra, dec)
-            for ra, dec in gaia_source_coords
-        ]
+        [input_dm.meta.wcs.world_to_pixel(ra, dec) for ra, dec in gaia_source_coords]
     )
     return catalog_data
 
@@ -586,9 +539,7 @@ def test_tweakreg_correction_magnitude(
     step = TweakRegStep()
     step.tolerance = tolerance / 10.0
 
-    assert (
-        step._is_wcs_correction_small(img1_wcs, img2_wcs) == is_small_correction
-    )
+    assert step._is_wcs_correction_small(img1_wcs, img2_wcs) == is_small_correction
 
 
 @pytest.mark.parametrize(
@@ -661,9 +612,7 @@ def test_tweakreg_save_valid_abs_refcat(tmp_path, abs_refcat, request):
     img = request.getfixturevalue("base_image")(shift_1=1000, shift_2=1000)
     catalog_filename = "ref_catalog.ecsv"
     abs_refcat_filename = f"fit_{abs_refcat.lower()}_ref.ecsv"
-    add_tweakreg_catalog_attribute(
-        tmp_path, img, catalog_filename=catalog_filename
-    )
+    add_tweakreg_catalog_attribute(tmp_path, img, catalog_filename=catalog_filename)
 
     step = TweakRegStep()
     step.save_abs_catalog = True
@@ -687,9 +636,7 @@ def test_tweakreg_defaults_to_valid_abs_refcat(tmp_path, abs_refcat, request):
     img = request.getfixturevalue("base_image")(shift_1=1000, shift_2=1000)
     catalog_filename = "ref_catalog.ecsv"
     abs_refcat_filename = f"fit_{DEFAULT_ABS_REFCAT.lower()}_ref.ecsv"
-    add_tweakreg_catalog_attribute(
-        tmp_path, img, catalog_filename=catalog_filename
-    )
+    add_tweakreg_catalog_attribute(tmp_path, img, catalog_filename=catalog_filename)
 
     step = TweakRegStep()
     step.save_abs_catalog = True
@@ -776,9 +723,7 @@ def test_tweakreg_use_custom_catalogs(tmp_path, catalog_format, request):
     catfile_content = StringIO()
     for x in custom_catalog_map:
         # write line to catfile
-        catfile_content.write(
-            f"{x.get('cat_datamodel')} {x.get('cat_filename')}\n"
-        )
+        catfile_content.write(f"{x.get('cat_datamodel')} {x.get('cat_filename')}\n")
         # write out the catalog data
         t = table.Table(x.get("cat_data"), names=("x", "y"))
         t.write(tmp_path / x.get("cat_filename"), format=catalog_format)
@@ -825,9 +770,7 @@ def test_tweakreg_rotated_plane(tmp_path, theta, offset_x, offset_y, request):
     Test that TweakReg returns accurate results.
     """
     gaia_cat = get_catalog(ra=270, dec=66, sr=100 / 3600)
-    gaia_source_coords = [
-        (ra, dec) for ra, dec in zip(gaia_cat["ra"], gaia_cat["dec"])
-    ]
+    gaia_source_coords = [(ra, dec) for ra, dec in zip(gaia_cat["ra"], gaia_cat["dec"])]
 
     img = request.getfixturevalue("base_image")(shift_1=1000, shift_2=1000)
     original_wcs = copy.deepcopy(img.meta.wcs)
@@ -857,13 +800,11 @@ def test_tweakreg_rotated_plane(tmp_path, theta, offset_x, offset_y, request):
 
     # get world coords for Gaia sources using "wrong WCS"
     original_ref_source = [
-        original_wcs.pixel_to_world(x, y)
-        for x, y in transformed_xy_gaia_sources
+        original_wcs.pixel_to_world(x, y) for x, y in transformed_xy_gaia_sources
     ]
     # get world coords for Gaia sources using tweaked WCS
     new_ref_source = [
-        img.meta.wcs.pixel_to_world(x, y)
-        for x, y in transformed_xy_gaia_sources
+        img.meta.wcs.pixel_to_world(x, y) for x, y in transformed_xy_gaia_sources
     ]
     # celestial coordinates for Gaia sources
     gaia_ref_source = [
@@ -883,9 +824,7 @@ def test_tweakreg_rotated_plane(tmp_path, theta, offset_x, offset_y, request):
         for gref, nref in zip(gaia_ref_source, new_ref_source)
     ]
 
-    assert np.array(
-        [np.less_equal(d2, d1) for d1, d2 in zip(dist1, dist2)]
-    ).all()
+    assert np.array([np.less_equal(d2, d1) for d1, d2 in zip(dist1, dist2)]).all()
 
 
 @pytest.mark.parametrize(
