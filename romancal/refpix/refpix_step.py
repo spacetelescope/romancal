fcf7ef7a8e91714f9b3d770a6a689657e2b7444e
William Jamieson <wjamieson@stsci.edu>
2023-07-03 17:23:23 -0400

    Fix pre-mature model close

--- romancal/refpix/refpix_step.py
+++ romancal/refpix/refpix_step.py
@@ -37,19 +37,20 @@ class RefPixStep(RomanStep):
 
         # open the input data model
         log.debug(f"Opening the science data: {input}")
-        with rdm.open(input) as datamodel:
-            # Get the reference file
-            ref_file = self.get_reference_file(datamodel, "refpix")
+        datamodel = rdm.open(input)
 
-            log.debug(f"Opening the reference file: {ref_file}")
-            with rdm.open(ref_file) as refs:
-                # Run the correction
-                log.debug("Running the reference pixel correction")
-                return refpix.run_steps(
-                    datamodel,
-                    refs,
-                    self.remove_offset,
-                    self.remove_trends,
-                    self.cosine_interpolate,
-                    self.fft_interpolate,
-                )
+        # Get the reference file
+        ref_file = self.get_reference_file(datamodel, "refpix")
+
+        log.debug(f"Opening the reference file: {ref_file}")
+        with rdm.open(ref_file) as refs:
+            # Run the correction
+            log.debug("Running the reference pixel correction")
+            return refpix.run_steps(
+                datamodel,
+                refs,
+                self.remove_offset,
+                self.remove_trends,
+                self.cosine_interpolate,
+                self.fft_interpolate,
+            )
