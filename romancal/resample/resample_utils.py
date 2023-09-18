81dd97ba76d7f4cf8e06a9df88fb705d30830e7d
William Jamieson <wjamieson@stsci.edu>
2023-09-18 10:03:48 -0400

    Add NumPy ruff rule set checking (#863)

--- romancal/resample/resample_utils.py
+++ romancal/resample/resample_utils.py
@@ -80,7 +80,7 @@ def make_output_wcs(
     )
 
     # Check that the output data shape has no zero length dimensions
-    if not np.product(output_wcs.array_shape):
+    if not np.prod(output_wcs.array_shape):
         raise ValueError(
             f"Invalid output frame shape: {tuple(output_wcs.array_shape)}; dimensions"
             " must have length > 0."
