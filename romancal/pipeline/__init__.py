68bada4c74b466a65cad3d4b58c58d0da15d5332
Dave Davis <ddavis@stsci.edu>
2023-11-07 12:00:32 -0500

    rcal-687 Update init and rename files

--- romancal/pipeline/__init__.py
+++ romancal/pipeline/__init__.py
@@ -3,5 +3,6 @@ This module collects all of the stpipe.Pipeline subclasses
 made available by this package.
 """
 from .exposure_pipeline import ExposurePipeline
+from .highlevel_pipeline import HighLevelPipeline
 
 __all__ = ["ExposurePipeline", "HighLevelPipeline"]
