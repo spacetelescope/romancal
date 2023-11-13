09893a300fd845ee266fb21b8d2f3e8a8e68dc9e
William Jamieson <wjamieson@stsci.edu>
2023-11-13 15:04:04 -0500

    Add big data regression test for jump detection step

--- romancal/regtest/test_jump_det.py
+++ romancal/regtest/test_jump_det.py
@@ -30,3 +30,50 @@ def test_jump_detection_step(rtdata, ignore_asdf_paths):
     rtdata.get_truth(f"truth/WFI/image/{output}")
     diff = compare_asdf(rtdata.output, rtdata.truth, **ignore_asdf_paths)
     assert diff.identical, diff.report()
+
+
+@pytest.mark.bigdata
+@pytest.mark.parametrize(
+    "base_name",
+    [
+        # "MLS0R1_alpha_1000_A_1000",
+        "MLS0R1_alpha_1000_A_0",
+        # "MLS0R1_alpha_1000_A_500",
+        "MLS0R1_alpha_10_A_200",
+        "MLS0R1_alpha_10_A_100",
+        "MLS0R1_alpha_10_A_0",
+    ],
+)
+def test_cas22_jump_detection(rtdata, ignore_asdf_paths, base_name):
+    input_file = f"{base_name}_input.asdf"
+    gain_file = f"{base_name}_gain.asdf"
+    readnoise_file = f"{base_name}_readnoise.asdf"
+
+    rtdata.get_data(f"WFI/image/{input_file}")
+    rtdata.get_data(f"WFI/image/{gain_file}")
+    rtdata.get_data(f"WFI/image/{readnoise_file}")
+    rtdata.input = input_file
+    rtdata.gain = gain_file
+    rtdata.readnoise = readnoise_file
+
+    args = [
+        "romancal.step.RampFitStep",
+        rtdata.input,
+        f"--override_gain={rtdata.gain}",
+        f"--override_readnoise={rtdata.readnoise}",
+        "--use_jump_detection=True",
+    ]
+    RomanStep.from_cmdline(args)
+    output = f"{base_name}_input_rampfit.asdf"
+    rtdata.output = output
+    rtdata.get_truth(f"truth/WFI/image/{base_name}_output.asdf")
+
+    import asdf
+
+    truth = asdf.open(rtdata.truth)["roman"]
+    output = asdf.open(rtdata.output)["roman"]
+
+    from numpy.testing import assert_allclose
+
+    assert_allclose(truth["dq"], output["dq"])
+    assert_allclose(truth["data"], output["data"])
