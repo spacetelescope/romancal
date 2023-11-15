da0b8e7f79e92c72cf3b6113fb4ead0b07b92875
William Jamieson <wjamieson@stsci.edu>
2023-11-15 09:53:17 -0500

    Disable jump detection steps completely

--- romancal/regtest/test_ramp_fitting.py
+++ romancal/regtest/test_ramp_fitting.py
@@ -10,19 +10,18 @@ from .regtestdata import compare_asdf
 @pytest.mark.bigdata
 def test_ramp_fitting_step(rtdata, ignore_asdf_paths):
     """Testing the ramp fitting step"""
-    input_data = "r0000101001001001001_01101_0001_WFI01_jump.asdf"
+    input_data = "r0000101001001001001_01101_0001_WFI01_darkcurrent.asdf"
     rtdata.get_data(f"WFI/image/{input_data}")
     rtdata.input = input_data
 
     args = [
         "romancal.step.RampFitStep",
         rtdata.input,
-        # Disable the jump detection in the rampfit step so this one will run
-        "--use_ramp_jump_detection=False",
     ]
     RomanStep.from_cmdline(args)
     output = "r0000101001001001001_01101_0001_WFI01_rampfit.asdf"
     rtdata.output = output
     rtdata.get_truth(f"truth/WFI/image/{output}")
+
     diff = compare_asdf(rtdata.output, rtdata.truth, **ignore_asdf_paths)
     assert diff.identical, diff.report()
