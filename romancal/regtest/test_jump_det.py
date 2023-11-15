2144516f3ae06406e0c55985b664548e4e276d98
William Jamieson <wjamieson@stsci.edu>
2023-11-14 20:10:20 -0500

    Create a standard "regression-test"

--- romancal/regtest/test_jump_det.py
+++ romancal/regtest/test_jump_det.py
@@ -23,6 +23,8 @@ def test_jump_detection_step(rtdata, ignore_asdf_paths):
         "--rejection_threshold=180.",
         "--three_group_rejection_threshold=185.",
         "--four_group_rejection_threshold=190.",
+        # disable the jump detection in the rampfit step so this one will run
+        "--run_ramp_jump_detection=False",
     ]
     RomanStep.from_cmdline(args)
     output = "r0000101001001001001_01101_0001_WFI01_jump.asdf"
@@ -33,40 +35,22 @@ def test_jump_detection_step(rtdata, ignore_asdf_paths):
 
 
 @pytest.mark.bigdata
-@pytest.mark.parametrize(
-    "base_name",
-    [
-        # "MLS0R1_alpha_1000_A_1000",
-        "MLS0R1_alpha_1000_A_0",
-        # "MLS0R1_alpha_1000_A_500",
-        "MLS0R1_alpha_10_A_200",
-        "MLS0R1_alpha_10_A_100",
-        "MLS0R1_alpha_10_A_0",
-    ],
-)
-def test_cas22_jump_detection(rtdata, ignore_asdf_paths, base_name):
-    input_file = f"{base_name}_input.asdf"
-    gain_file = f"{base_name}_gain.asdf"
-    readnoise_file = f"{base_name}_readnoise.asdf"
-
+def test_cas22_jump_detection(rtdata, ignore_asdf_paths):
+    input_file = "r0000101001001001001_01101_0001_WFI01_darkcurrent.asdf"
     rtdata.get_data(f"WFI/image/{input_file}")
-    rtdata.get_data(f"WFI/image/{gain_file}")
-    rtdata.get_data(f"WFI/image/{readnoise_file}")
     rtdata.input = input_file
-    rtdata.gain = gain_file
-    rtdata.readnoise = readnoise_file
-
     args = [
         "romancal.step.RampFitStep",
         rtdata.input,
-        f"--override_gain={rtdata.gain}",
-        f"--override_readnoise={rtdata.readnoise}",
+        # Ensure the jump detection is run in the rampfit step
         "--use_ramp_jump_detection=True",
+        # Add differentiating suffix to output file (so it doesn't collide with rampfit)
+        "--suffix=rampfit_jump",
     ]
     RomanStep.from_cmdline(args)
-    output = f"{base_name}_input_rampfit.asdf"
+    output = "r0000101001001001001_01101_0001_WFI01_rampfit_jump.asdf"
     rtdata.output = output
-    rtdata.get_truth(f"truth/WFI/image/{base_name}_output.asdf")
+    rtdata.get_truth(f"truth/WFI/image/{output}")
 
     diff = compare_asdf(rtdata.output, rtdata.truth, **ignore_asdf_paths)
     assert diff.identical, diff.report()
