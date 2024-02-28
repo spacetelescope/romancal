f80ac22f259a63d5072986c04a89b661ab1cad99
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2024-02-28 15:51:28 -0500

    actually apply the flux correction
    tests now pass

--- romancal/flux/flux_step.py
+++ romancal/flux/flux_step.py
@@ -1,7 +1,15 @@
 """Apply the flux scaling"""
+import logging
+
+import astropy.units as u
 
 from ..datamodels import ModelContainer
 from ..stpipe import RomanStep
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+__all__ = ["FluxStep"]
 
 
 class FluxStep(RomanStep):
@@ -24,7 +32,7 @@ class FluxStep(RomanStep):
 
     Notes
     -----
-    Currently, the application is done in-place; the inputs are directly modified if in-memory DataModels are input.
+    Currently, the correction is done in-place; the inputs are directly modified if in-memory DataModels are input.
     """  # noqa: E501
 
     class_alias = 'flux'
@@ -35,4 +43,43 @@ class FluxStep(RomanStep):
     reference_file_types = []
 
     def process(self, input):
+        apply_flux_correction(input)
+
         return input
+
+
+def apply_flux_correction(model):
+    """Apply the flux correction
+
+    The input model is expected to be Roman ImageModel-like.
+    The photometry information is taken from `meta.photometry`.
+
+    The model is converted in-place.
+
+    Parameters
+    ----------
+    model : `ImageModel`
+        The model to apply the flux correction to. The model is modified in-place.
+
+    Notes
+    -----
+    The modifications to the model can result in validation issues due to change of units.
+    """
+    # Define the various variance arrays
+    VARIANCES = ('var_rnoise', 'var_poisson', 'var_flat')
+
+    # Check for units. Must be election/second. Otherwise, it is unknown how to
+    # convert.
+    if model.data.unit != u.electron / u.s:
+        log.debug('Input data is not in units of e/s. Flux correction will not be done.')
+        log.debug('Input data units are %s', model.data.unit)
+        return
+
+    # Apply the correction
+    # Assignments into the model are done through `_instance` to avoid
+    # validation errors on the units.
+    log.debug('Flux correction being applied')
+    c_mj = model.meta.photometry.conversion_megajanskys
+    model._instance['data'] = model.data * c_mj
+    for variance in VARIANCES:
+        model._instance[variance] = getattr(model, variance) * c_mj**2
