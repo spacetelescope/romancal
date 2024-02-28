b8b90a7bd1603cd6fc4fb817397413f2473b0055
M. Teodoro <mteodoro@stsci.edu>
2024-02-28 11:38:23 -0500

    Add unit test for testing outlier detection capability.

--- romancal/outlier_detection/tests/test_outlier_detection.py
+++ romancal/outlier_detection/tests/test_outlier_detection.py
@@ -1,10 +1,38 @@
 import os
 from pathlib import Path
 
+import numpy as np
 import pytest
+from astropy.units import Quantity
 
 from romancal.datamodels import ModelContainer
 from romancal.outlier_detection import OutlierDetectionStep, outlier_detection
+
+
+@pytest.fixture()
+def clean_up_after_test():
+    """
+    Clean up working directory after test.
+    """
+
+    def _clean_up_working_directory(pattern="*.asdf"):
+        """
+        Clean up the working directory by removing files matching the specified pattern.
+        Parameters
+        ----------
+        pattern : str, optional
+            The pattern of the file pattern to match (default is "*.asdf")
+
+        Returns
+        -------
+        None
+
+        """
+        for x in (Path(os.getcwd())).glob(pattern):
+            if x.exists():
+                x.unlink()
+
+    return _clean_up_working_directory
 
 
 @pytest.mark.parametrize(
@@ -152,7 +180,9 @@ def test_outlier_init_default_parameters(pars, base_image):
     assert step.resample_suffix == f"_outlier_{pars['resample_suffix']}.asdf"
 
 
-def test_outlier_do_detection(tmp_path, base_image):
+def test_outlier_do_detection_write_files_to_custom_location(
+    tmp_path, base_image, clean_up_after_test
+):
     """
     Test that OutlierDetection can create files on disk in a custom location.
     """
@@ -207,7 +237,80 @@ def test_outlier_do_detection(tmp_path, base_image):
 
     assert all(x.exists() for x in outlier_files_path)
 
-    # delete temporary resample file
-    for x in (Path(os.getcwd())).glob("*_outlier*.asdf"):
-        if x.exists():
-            x.unlink()
+    clean_up_after_test("*.asdf")
+
+
+def test_outlier_do_detection_find_outliers(tmp_path, base_image, clean_up_after_test):
+    """
+    Test that OutlierDetection can find outliers.
+    """
+    img_1 = base_image()
+    img_1.meta.filename = "img_1.asdf"
+    img_2 = base_image()
+    img_2.meta.filename = "img_2.asdf"
+
+    # add outliers
+    img_1_input_coords = np.array(
+        [(5, 45), (25, 25), (45, 85), (65, 65), (85, 5)], dtype=[("x", int), ("y", int)]
+    )
+    img_2_input_coords = np.array(
+        [(15, 25), (35, 5), (75, 65), (95, 45), (99, 5)], dtype=[("x", int), ("y", int)]
+    )
+    img_1.data[img_1_input_coords["x"], img_1_input_coords["y"]] = Quantity(
+        100000, "electron / s"
+    )
+    img_2.data[img_2_input_coords["x"], img_2_input_coords["y"]] = Quantity(
+        100000, "electron / s"
+    )
+
+    input_models = ModelContainer([img_1, img_2])
+
+    outlier_step = OutlierDetectionStep()
+    # set output dir for all files created by the step
+    outlier_step.output_dir = tmp_path.as_posix()
+    # make sure files are written out to disk
+    outlier_step.in_memory = False
+
+    pars = {
+        "weight_type": "exptime",
+        "pixfrac": 1.0,
+        "kernel": "square",
+        "fillval": "INDEF",
+        "nlow": 0,
+        "nhigh": 0,
+        "maskpt": 0.7,
+        "grow": 1,
+        "snr": "4.0 3.0",
+        "scale": "0.5 0.4",
+        "backg": 0.0,
+        "kernel_size": "7 7",
+        "save_intermediate_results": False,
+        "resample_data": True,
+        "good_bits": 0,
+        "allowed_memory": None,
+        "in_memory": outlier_step.in_memory,
+        "make_output_path": outlier_step.make_output_path,
+        "resample_suffix": "i2d",
+    }
+
+    detection_step = outlier_detection.OutlierDetection
+    step = detection_step(input_models, **pars)
+
+    step.do_detection()
+
+    # get flagged outliers coordinates from DQ array
+    img_1_outlier_output_coords = np.where(step.input_models[0].dq > 0)
+
+    # reformat output and input coordinates and sort by x coordinate
+    outliers_output_coords = np.array(
+        [(x, y) for x, y in zip(*img_1_outlier_output_coords)],
+        dtype=[("x", int), ("y", int)],
+    )
+    outliers_input_coords = np.concatenate((img_1_input_coords, img_2_input_coords))
+
+    outliers_output_coords.sort(axis=0)
+    outliers_input_coords.sort(axis=0)
+
+    assert all(outliers_input_coords == outliers_output_coords)
+
+    clean_up_after_test("*.asdf")
