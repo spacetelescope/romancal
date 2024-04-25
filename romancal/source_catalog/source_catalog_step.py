17c521880ebfd9dea4ac4fde51ea93e8ff6d6113
Larry Bradley <larry.bradley@gmail.com>
2024-04-25 13:53:51 -0400

    Update source_catalog_step module

--- romancal/source_catalog/source_catalog_step.py
+++ romancal/source_catalog/source_catalog_step.py
@@ -4,34 +4,35 @@ Module for the source catalog step.
 
 import numpy as np
 from astropy.table import Table
-from crds.core.exceptions import CrdsLookupError
 from roman_datamodels import datamodels, maker_utils
 
+from romancal.source_catalog.background import RomanBackground
+from romancal.source_catalog.detection import convolve_data, make_segmentation_image
+from romancal.source_catalog.reference_data import ReferenceData
+from romancal.source_catalog.source_catalog import RomanSourceCatalog
 from romancal.stpipe import RomanStep
-
-from .detection import RomanBackground, RomanSourceFinder, convolve_data
-from .reference_data import ReferenceData
-from .source_catalog import RomanSourceCatalog
 
 __all__ = ["SourceCatalogStep"]
 
 
 class SourceCatalogStep(RomanStep):
     """
-    Create a final catalog of source photometry and morphologies.
+    Create a catalog of sources including photometry and basic shape
+    measurements.
 
     Parameters
     -----------
-    input : str or `ImageModel`
-        Path to an ASDF file, or an `ImageModel`.
+    input : str or `MosaicModel`
+        Path to an ASDF file or an `MosaicModel`.
     """
 
     class_alias = "source_catalog"
+    reference_file_types = []
 
     spec = """
         bkg_boxsize = integer(default=1000)   # background mesh box size in pixels
         kernel_fwhm = float(default=2.0)      # Gaussian kernel FWHM in pixels
-        snr_threshold = float(default=3.0)    # SNR threshold above the bkg
+        snr_threshold = float(default=3.0)    # per-pixel SNR threshold above the bkg
         npixels = integer(default=25)         # min number of pixels in source
         deblend = boolean(default=False)      # deblend sources?
         aperture_ee1 = integer(default=30)    # aperture encircled energy 1
@@ -42,57 +43,40 @@ class SourceCatalogStep(RomanStep):
         suffix = string(default='cat')        # Default suffix for output files
     """
 
-    def _get_reffile_paths(self, model):
-        filepaths = []
-        for reffile_type in self.reference_file_types:
-            try:
-                filepath = self.get_reference_file(model, reffile_type)
-                self.log.info(
-                    f"Using {reffile_type.upper()} reference file: " f"{filepath}"
-                )
-            except CrdsLookupError as err:
-                msg = f"{err} Source catalog will not be created."
-                self.log.warning(msg)
-                return None
-
-            filepaths.append(filepath)
-        return filepaths
-
     def process(self, input_model):
         with datamodels.open(input_model) as model:
-            reffile_paths = self._get_reffile_paths(model)
+            source_catalog_model = maker_utils.mk_datamodel(
+                datamodels.MosaicSourceCatalogModel
+            )
+
             aperture_ee = (self.aperture_ee1, self.aperture_ee2, self.aperture_ee3)
+            refdata = ReferenceData(model, aperture_ee)
+            aperture_params = refdata.aperture_params
 
-            try:
-                refdata = ReferenceData(model, reffile_paths, aperture_ee)
-                aperture_params = refdata.aperture_params
-
-                # TODO (@bmorris3): replace or remove
-                abvega_offset = refdata.abvega_offset
-            except RuntimeError as err:
-                msg = f"{err} Source catalog will not be created."
-                self.log.warning(msg)
-                return None
-
-            coverage_mask = np.isnan(model.err)  # | (model.wht == 0)
+            mask = np.isnan(model.data)
+            coverage_mask = np.isnan(model.err)
             bkg = RomanBackground(
-                model.data, box_size=self.bkg_boxsize, coverage_mask=coverage_mask
+                model.data,
+                box_size=self.bkg_boxsize,
+                mask=mask,
+                coverage_mask=coverage_mask,
             )
             model.data -= bkg.background
 
-            threshold = self.snr_threshold * bkg.background_rms
-            finder = RomanSourceFinder(threshold, self.npixels, deblend=self.deblend)
-
             convolved_data = convolve_data(
-                model.data, self.kernel_fwhm, mask=coverage_mask
-            )
-            segment_img = finder(convolved_data, mask=coverage_mask)
-
-            source_catalog_model = maker_utils.mk_datamodel(
-                datamodels.SourceCatalogModel
+                model.data, kernel_fwhm=self.kernel_fwhm, mask=coverage_mask
             )
 
-            if segment_img is None:
+            segment_img = make_segmentation_image(
+                convolved_data,
+                snr_threshold=self.snr_threshold,
+                npixels=self.npixels,
+                bkg_rms=bkg.background_rms,
+                deblend=self.deblend,
+                mask=coverage_mask,
+            )
+
+            if segment_img is None:  # no sources found
                 source_catalog_model.source_catalog = Table()
                 return source_catalog_model
 
@@ -101,34 +85,30 @@ class SourceCatalogStep(RomanStep):
                 model,
                 segment_img,
                 convolved_data,
-                self.kernel_fwhm,
                 aperture_params,
-                abvega_offset,
                 ci_star_thresholds,
+                self.kernel_fwhm,
             )
-            catalog = catobj.catalog
+
+            # put the resulting catalog in the model
+            source_catalog_model.source_catalog = catobj.catalog
 
             # add back background to data so input model is unchanged
+            # (in case of interactive use)
             model.data += bkg.background
 
             if self.save_results:
-                cat_filepath = self.make_output_path(ext=".ecsv")
-                catalog.write(cat_filepath, format="ascii.ecsv", overwrite=True)
-                self.log.info(f"Wrote source catalog: {cat_filepath}")
+                # NOTE: the source_catalog_model is automatically saved
+                #       if save_results = True
 
+                # save the segmentation map
                 segmentation_model = maker_utils.mk_datamodel(
-                    datamodels.SegmentationMapModel
+                    datamodels.MosaicSegmentationMapModel
                 )
+                segmentation_model.data = segment_img.data.astype(np.uint32)
                 self.save_model(segmentation_model, suffix="segm")
 
-                # TODO (@bmorris3): replace or remove
-                # model.meta.segmentation_map = segmentation_model.meta.filename
-
-                self.log.info(
-                    "Wrote segmentation map: " f"{segmentation_model.meta.filename}"
-                )
-
-        # put the resulting catalog in the model:
-        source_catalog_model.source_catalog = catalog
-
+        # ~/files/dev/repos/dev/romancal/romancal/stpipe/core.py:67
+        # AttributeError: No such attribute (ref_file) found in node
+        # nden: schema needs to be updated
         return source_catalog_model
