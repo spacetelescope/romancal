851ec934e989b50c2276d98438ff09440b0a052c
Dave Davis <ddavis@stsci.edu>
2024-05-13 11:35:53 -0400

    rcal-827 Update file names

--- romancal/regtest/test_mos_pipeline.py
+++ romancal/regtest/test_mos_pipeline.py
@@ -38,7 +38,7 @@ def test_level3_mos_pipeline(rtdata, ignore_asdf_paths):
     rtdata.input = input_asn
 
     # Test Pipeline
-    output = "r0099101001001001001_F158_visit_i2d.asdf"
+    output = "r0099101001001001001_F158_prompt_visit_i2d.asdf"
     rtdata.output = output
     args = [
         "roman_mos",
@@ -50,15 +50,15 @@ def test_level3_mos_pipeline(rtdata, ignore_asdf_paths):
     assert diff.identical, diff.report()
 
     # Generate thumbnail image
-    input_file = "r0099101001001001001_F158_visit_i2d.asdf"
-    thumbnail_file = "r0099101001001001001_F158_visit_thumb.png"
+    input_file = "r0099101001001001001_F158_prompt_visit_i2d.asdf"
+    thumbnail_file = "r0099101001001001001_F158_prompt_visit_thumb.png"
 
     preview_cmd = f"stpreview to {input_file} {thumbnail_file} 256 256 roman"
     os.system(preview_cmd)  # nosec
 
     # Generate preview image
-    input_file = "r0099101001001001001_F158_visit_i2d.asdf"
-    preview_file = "r0099101001001001001_F158_visit_preview.png"
+    input_file = "r0099101001001001001_F158_prompt_visit_i2d.asdf"
+    preview_file = "r0099101001001001001_F158_prompt_visit_preview.png"
     preview_cmd = f"stpreview to {input_file} {preview_file} 1080 1080 roman"
     os.system(preview_cmd)  # nosec
 
@@ -142,7 +142,7 @@ def test_hlp_mosaic_pipeline(rtdata, ignore_asdf_paths):
         "WFI/image/r0000101001001001001_01101_0003_WFI01_cal.asdf",
     ]
 
-    asn_cmd = f"asn_from_list --product-name='r0099101001001001001_F158_visit_r274dp63x31y81'\
+    asn_cmd = f"asn_from_list --product-name='r0099101001001001001_F158_prompt_visit_r274dp63x31y81'\
     r0000101001001001001_01101_0001_WFI01_cal.asdf \
     r0000101001001001001_01101_0002_WFI01_cal.asdf \
     r0000101001001001001_01101_0003_WFI01_cal.asdf \
@@ -157,7 +157,7 @@ def test_hlp_mosaic_pipeline(rtdata, ignore_asdf_paths):
     rtdata.input = input_asn
 
     # Test Pipeline
-    output = "r0099101001001001001_F158_visit_r274dp63x31y81_i2d.asdf"
+    output = "r0099101001001001001_F158_prompt_visit_r274dp63x31y81_i2d.asdf"
     rtdata.output = output
     args = [
         "--disable-crds-steppars",
