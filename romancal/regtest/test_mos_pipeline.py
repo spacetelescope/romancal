303af972c0c3e15e5f4da12c2691920101a29395
Dave Davis <ddavis@stsci.edu>
2024-05-13 14:41:16 -0400

    rcal-827 Update file names in level3_hlp & tests

--- romancal/regtest/test_mos_pipeline.py
+++ romancal/regtest/test_mos_pipeline.py
@@ -50,14 +50,14 @@ def test_level3_mos_pipeline(rtdata, ignore_asdf_paths):
     assert diff.identical, diff.report()
 
     # Generate thumbnail image
-    input_file = "r0099101001001001001_F158_prompt_visit_r274dp63x31y8_i2d.asdf"
+    input_file = "r0099101001001001001_F158_prompt_visit_r274dp63x31y81_i2d.asdf"
     thumbnail_file = "r0099101001001001001_F158_prompt_visit_r274dp63x31y81_thumb.png"
 
     preview_cmd = f"stpreview to {input_file} {thumbnail_file} 256 256 roman"
     os.system(preview_cmd)  # nosec
 
     # Generate preview image
-    input_file = "r0099101001001001001_F158_prompt_visit_r274dp63x31y8_i2d.asdf"
+    input_file = "r0099101001001001001_F158_prompt_visit_r274dp63x31y81_i2d.asdf"
     preview_file = "r0099101001001001001_F158_prompt_visit_r274dp63x31y81_preview.png"
     preview_cmd = f"stpreview to {input_file} {preview_file} 1080 1080 roman"
     os.system(preview_cmd)  # nosec
@@ -142,12 +142,13 @@ def test_hlp_mosaic_pipeline(rtdata, ignore_asdf_paths):
         "WFI/image/r0000101001001001001_01101_0003_WFI01_cal.asdf",
     ]
 
+    # skip this creation for the moment - need to update asn_from_list
     asn_cmd = f"asn_from_list --product-name='r0099101001001001001_F158_prompt_visit_r274dp63x31y81'\
     r0000101001001001001_01101_0001_WFI01_cal.asdf \
     r0000101001001001001_01101_0002_WFI01_cal.asdf \
     r0000101001001001001_01101_0003_WFI01_cal.asdf \
     -o L3_m1_asn.json"
-    os.system(asn_cmd)  # nosec
+    #os.system(asn_cmd)  # nosec
     
     for cal_file in cal_files:
         rtdata.get_data(cal_file)
