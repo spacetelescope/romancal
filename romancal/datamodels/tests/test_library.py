4392df70c42a2f4e8456cbb792fbd1cbadc28ebb
Brett <brettgraham@gmail.com>
2024-06-17 14:47:39 -0400

    use library from stpipe

--- romancal/datamodels/tests/test_library.py
+++ romancal/datamodels/tests/test_library.py
@@ -1,6 +1,4 @@
 import json
-import os
-from contextlib import nullcontext
 
 import pytest
 import roman_datamodels.datamodels as dm
@@ -8,7 +6,7 @@ from roman_datamodels.maker_utils import mk_level2_image
 
 from romancal.associations import load_asn
 from romancal.associations.asn_from_list import asn_from_list
-from romancal.datamodels.library import BorrowError, ClosedLibraryError, ModelLibrary
+from romancal.datamodels.library import ModelLibrary
 
 # for the example association, set 2 different observation numbers
 # so the association will have 2 groups (since all other group_id
@@ -70,74 +68,17 @@ def _set_custom_member_attr(example_asn_path, member_index, attr, value):
         json.dump(asn_data, f)
 
 
-def test_load_asn(example_library):
-    """
-    Test that __len__ returns the number of models/members loaded
-    from the association (and does not require opening the library)
-    """
-    assert len(example_library) == _N_MODELS
+def test_assign_member(example_asn_path):
+    exptypes = ["science"] * _N_MODELS
+    _set_custom_member_attr(example_asn_path, 1, "exptype", "background")
+    exptypes[1] = "background"
 
+    def get_exptype(model, index):
+        return model.meta.exptype.lower()
 
-def test_init_from_asn(example_asn_path):
-    with open(example_asn_path) as f:
-        asn = load_asn(f)
-    # as association filenames are local we must be in the same directory
-    os.chdir(example_asn_path.parent)
-    lib = ModelLibrary(asn)
-    assert len(lib) == _N_MODELS
+    library = ModelLibrary(example_asn_path)
 
-
-@pytest.mark.parametrize("asn_n_members", range(_N_MODELS))
-def test_asn_n_members(example_asn_path, asn_n_members):
-    """
-    Test that creating a library with a `asn_n_members` filter
-    includes only the first N members
-    """
-    library = ModelLibrary(example_asn_path, asn_n_members=asn_n_members)
-    assert len(library) == asn_n_members
-
-
-def test_asn_exptypes(example_asn_path):
-    """
-    Test that creating a library with a `asn_exptypes` filter
-    includes only the members with a matching `exptype`
-    """
-    _set_custom_member_attr(example_asn_path, 0, "exptype", "background")
-    library = ModelLibrary(example_asn_path, asn_exptypes="science")
-    assert len(library) == _N_MODELS - 1
-    library = ModelLibrary(example_asn_path, asn_exptypes="background")
-    assert len(library) == 1
-
-
-def test_group_names(example_library):
-    """
-    Test that `group_names` returns appropriate names
-    based on the inferred group ids and that these names match
-    the `model.meta.group_id` values
-    """
-    assert len(example_library.group_names) == _N_GROUPS
-    group_names = set()
-    with example_library:
-        for index, model in enumerate(example_library):
-            group_names.add(model.meta.group_id)
-            example_library.shelve(model, index, modify=False)
-    assert group_names == set(example_library.group_names)
-
-
-def test_group_indices(example_library):
-    """
-    Test that `group_indices` returns appropriate model indices
-    based on the inferred group ids
-    """
-    group_indices = example_library.group_indices
-    assert len(group_indices) == _N_GROUPS
-    with example_library:
-        for group_name in group_indices:
-            indices = group_indices[group_name]
-            for index in indices:
-                model = example_library.borrow(index)
-                assert model.meta.group_id == group_name
-                example_library.shelve(model, index, modify=False)
+    assert list(library.map_function(get_exptype)) == exptypes
 
 
 @pytest.mark.parametrize("attr", ["group_names", "group_indices"])
@@ -160,243 +101,8 @@ def test_group_with_no_datamodels_open(example_asn_path, attr, monkeypatch):
     getattr(library, attr)
 
 
-# @pytest.mark.parametrize(
-#     "asn_group_id, meta_group_id, expected_group_id", [
-#         ('42', None, '42'),
-#         (None, '42', '42'),
-#         ('42', '26', '42'),
-#     ])
-# def test_group_id_override(example_asn_path, asn_group_id, meta_group_id, expected_group_id):
-#     """
-#     Test that overriding a models group_id via:
-#         - the association member entry
-#         - the model.meta.group_id
-#     overwrites the automatically calculated group_id (with the asn taking precedence)
-#     """
-#     if asn_group_id:
-#         _set_custom_member_attr(example_asn_path, 0, 'group_id', asn_group_id)
-#     if meta_group_id:
-#         model_filename = example_asn_path.parent / '0.fits'
-#         with dm.open(model_filename) as model:
-#             model.meta.group_id = meta_group_id
-#             model.save(model_filename)
-#     library = ModelLibrary(example_asn_path)
-#     group_names = library.group_names
-#     assert len(group_names) == 3
-#     assert expected_group_id in group_names
-#     with library:
-#         model = library[0]
-#         assert model.meta.group_id == expected_group_id
-#         library.discard(0, model)
-
-
-@pytest.mark.parametrize("modify", (True, False))
-def test_model_iteration(example_library, modify):
-    """
-    Test that iteration through models and shelving models
-    returns the appropriate models
-    """
-    with example_library:
-        for i, model in enumerate(example_library):
-            assert int(model.meta.filename.split(".")[0]) == i
-            example_library.shelve(model, i, modify=modify)
-
-
-@pytest.mark.parametrize("modify", (True, False))
-def test_model_indexing(example_library, modify):
-    """
-    Test that borrowing models (using __getitem__)  and returning (or discarding)
-    models returns the appropriate models
-    """
-    with example_library:
-        for i in range(_N_MODELS):
-            model = example_library.borrow(i)
-            assert int(model.meta.filename.split(".")[0]) == i
-            example_library.shelve(model, i, modify=modify)
-
-
-def test_closed_library_model_getitem(example_library):
-    """
-    Test that indexing a library when it is not open triggers an error
-    """
-    with pytest.raises(ClosedLibraryError, match="ModelLibrary is not open"):
-        example_library.borrow(0)
-
-
-def test_closed_library_model_iter(example_library):
-    """
-    Test that attempting to iterate a library that is not open triggers an error
-    """
-    with pytest.raises(ClosedLibraryError, match="ModelLibrary is not open"):
-        for model in example_library:
-            pass
-
-
-def test_double_borrow_by_index(example_library):
-    """
-    Test that double-borrowing a model (using __getitem__) results in an error
-    """
-    with pytest.raises(BorrowError, match="1 un-returned models"):
-        with example_library:
-            model0 = example_library.borrow(0)  # noqa: F841
-            with pytest.raises(BorrowError, match="Attempt to double-borrow model"):
-                model1 = example_library.borrow(0)  # noqa: F841
-
-
-def test_double_borrow_during_iter(example_library):
-    """
-    Test that double-borrowing a model (once via iter and once via __getitem__)
-    results in an error
-    """
-    with pytest.raises(BorrowError, match="1 un-returned models"):
-        with example_library:
-            for index, model in enumerate(example_library):
-                with pytest.raises(BorrowError, match="Attempt to double-borrow model"):
-                    model1 = example_library.borrow(index)  # noqa: F841
-                break
-
-
-@pytest.mark.parametrize("modify", (True, False))
-def test_non_borrowed(example_library, modify):
-    """
-    Test that attempting to shelve a non-borrowed item results in an error
-    """
-    with example_library:
-        with pytest.raises(BorrowError, match="Attempt to shelve non-borrowed model"):
-            example_library.shelve(None, 0, modify=modify)
-
-
-@pytest.mark.parametrize("n_borrowed", (1, 2))
-def test_no_return_getitem(example_library, n_borrowed):
-    """
-    Test that borrowing and not returning models results in an
-    error noting the number of un-returned models.
-    """
-    with pytest.raises(
-        BorrowError, match=f"ModelLibrary has {n_borrowed} un-returned models"
-    ):
-        with example_library:
-            for i in range(n_borrowed):
-                example_library.borrow(i)
-
-
-def test_exception_while_open(example_library):
-    """
-    Test that the __exit__ implementation for the library
-    passes exceptions that occur in the context
-    """
-    with pytest.raises(Exception, match="test"):
-        with example_library:
-            raise Exception("test")
-
-
-def test_exception_with_borrow(example_library):
-    """
-    Test that an exception while the library is open and has a borrowed
-    model results in the exception being raised (and not an exception
-    about a borrowed model not being returned).
-    """
-    with pytest.raises(Exception, match="test"):
-        with example_library:
-            model = example_library.borrow(0)  # noqa: F841
-            raise Exception("test")
-
-
 def test_asn_data(example_library):
     """
     Test that `asn` returns the association information
     """
     assert example_library.asn["products"][0]["name"] == _PRODUCT_NAME
-
-
-def test_asn_readonly(example_library):
-    """
-    Test that modifying the product (dict) in the `asn` result triggers an exception
-    """
-    with pytest.raises(TypeError, match="object does not support item assignment"):
-        example_library.asn["products"][0]["name"] = f"{_PRODUCT_NAME}_new"
-
-
-def test_asn_members_readonly(example_library):
-    """
-    Test that modifying members (list) in the `asn` result triggers an exception
-    """
-    with pytest.raises(TypeError, match="object does not support item assignment"):
-        example_library.asn["products"][0]["members"][0]["group_id"] = "42"
-
-
-def test_asn_members_tuple(example_library):
-    """
-    Test that even nested items in `asn` (like `members`) are immutable
-    """
-    assert isinstance(example_library.asn["products"][0]["members"], tuple)
-
-
-# def test_members(example_library):
-#     assert example_library.asn['products'][0]['members'] == example_library.members
-#
-#
-# def test_members_tuple(example_library):
-#     assert isinstance(example_library.members, tuple)
-
-
-@pytest.mark.parametrize("n, err", [(1, False), (2, True)])
-def test_stpipe_models_access(example_asn_path, n, err):
-    """
-    stpipe currently reaches into _models (but only when asn_n_members
-    is 1) so we support this `_models` attribute (with a loaded model)
-    only under that condition until stpipe can be updated to not reach
-    into `_models`.
-    """
-    library = ModelLibrary(example_asn_path, asn_n_members=n)
-    if err:
-        ctx = pytest.raises(AttributeError, match="object has no attribute '_models'")
-    else:
-        ctx = nullcontext()
-    with ctx:
-        assert library._models[0].get_crds_parameters()
-
-
-@pytest.mark.parametrize("modify", [True, False])
-def test_on_disk_model_modification(example_asn_path, modify):
-    """
-    Test that modifying a model in a library that is on_disk
-    does not persist if the model is shelved with modify=False
-    """
-    library = ModelLibrary(example_asn_path, on_disk=True)
-    with library:
-        model = library.borrow(0)
-        model.meta["foo"] = "bar"
-        library.shelve(model, 0, modify=modify)
-        model = library.borrow(0)
-        if modify:
-            assert getattr(model.meta, "foo") == "bar"
-        else:
-            assert getattr(model.meta, "foo", None) is None
-        # shelve the model so the test doesn't fail because of an un-returned
-        # model
-        library.shelve(0, model, modify=False)
-
-
-@pytest.mark.parametrize("on_disk", [True, False])
-def test_on_disk_no_overwrite(example_asn_path, on_disk):
-    """
-    Test that modifying a model in a library does not overwrite
-    the input file (even if on_disk==True)
-    """
-    library = ModelLibrary(example_asn_path, on_disk=on_disk)
-    with library:
-        model = library.borrow(0)
-        model.meta["foo"] = "bar"
-        library.shelve(model, 0)
-
-    library2 = ModelLibrary(example_asn_path, on_disk=on_disk)
-    with library2:
-        model = library2.borrow(0)
-        assert getattr(model.meta, "foo", None) is None
-        library2.shelve(model, 0)
-
-
-# TODO container conversion
-# TODO index
-# TODO memmap?
