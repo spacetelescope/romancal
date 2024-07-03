134bfb74b570e41ae0815987838e1e0d3753cd00
Dave Davis <ddavis@stsci.edu>
2024-07-03 15:52:45 -0400

    rcal-860 Unit test updates

--- romancal/datamodels/tests/test_filetype.py
+++ romancal/datamodels/tests/test_filetype.py
@@ -2,7 +2,10 @@ from pathlib import Path
 
 import pytest
 
+import roman_datamodels as rdm
 from romancal.datamodels import filetype
+from romancal.datamodels import ModelContainer
+
 
 DATA_DIRECTORY = Path(__file__).parent / "data"
 
@@ -20,6 +23,11 @@ def test_filetype():
     with open(DATA_DIRECTORY / "fake.json") as file_h:
         file_8 = filetype.check(file_h)
     file_9 = filetype.check(str(DATA_DIRECTORY / "pluto.asdf"))
+    model_container = ModelContainer()
+    file_10 = filetype.check(model_container)
+    image_node = rdm.maker_utils.mk_level2_image(shape=(20,20))
+    im1 = rdm.datamodels.ImageModel(image_node)
+    file_11 = filetype.check(im1)
 
     assert file_1 == "asn"
     assert file_2 == "asn"
@@ -30,6 +38,9 @@ def test_filetype():
     assert file_7 == "asdf"
     assert file_8 == "asn"
     assert file_9 == "asdf"
+    assert file_10 == "ModelContainer"
+    assert file_11 == "DataModel"
+    
 
     with pytest.raises(ValueError):
         filetype.check(DATA_DIRECTORY / "empty.txt")
