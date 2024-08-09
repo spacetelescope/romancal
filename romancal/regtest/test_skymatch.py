ee6a2f47e33bdc186f7e4283cb5c310537316e9b
M. Teodoro <mteodoro@stsci.edu>
2024-08-09 10:33:51 -0400

    Remove unset/unused variable.

--- romancal/regtest/test_skymatch.py
+++ romancal/regtest/test_skymatch.py
@@ -1,37 +1,133 @@
+import os
 import pytest
-from romancal.stpipe import RomanStep
 from roman_datamodels import datamodels as rdm
+from metrics_logger.decorators import metrics_logger
+from romancal.datamodels.library import ModelLibrary
+from romancal.pipeline.mosaic_pipeline import MosaicPipeline
+from romancal.skymatch import SkyMatchStep
+from romancal.stpipe import RomanStep
 
 from .regtestdata import compare_asdf
 
 
+def is_not_none_or_empty(value):
+    """
+    Check if a value is neither None nor empty.
+
+    Parameters
+    ----------
+    value : any
+        The value to check. It can be of any type.
+
+    Returns
+    -------
+    bool
+        True if the value is neither None nor empty, False otherwise.
+
+    Examples
+    --------
+    >>> is_not_none_or_empty(None)
+    False
+    >>> is_not_none_or_empty("")
+    False
+    >>> is_not_none_or_empty([])
+    False
+    >>> is_not_none_or_empty({})
+    False
+    >>> is_not_none_or_empty("Hello")
+    True
+    >>> is_not_none_or_empty([1, 2, 3])
+    True
+    >>> is_not_none_or_empty({"key": "value"})
+    True
+    """
+    if value is None:
+        return False
+    if isinstance(value, (str, list, dict, set, tuple)) and len(value) == 0:
+        return False
+    return True
+
+
+@metrics_logger()
 @pytest.mark.bigdata
-def test_skymatch_step(rtdata, ignore_asdf_paths):
-    """Test for the flat field step using imaging data."""
+def test_skymatch_step(rtdata):
+    """Test for the sky match step using imaging data."""
 
-    l2_data_folder = "/Users/mteodoro/ROMAN/SYNTHETIC_IMAGES/IMAGES/EDDIE/romanisim/20240311/PROCESSED/ELP"
-
-    input_data = [
-        "r0000101001001001001_01101_0001_WFI01_cal.asdf",
-        "r0000101001001001001_01101_0002_WFI01_cal.asdf",
-    ]
-    output_data = "skymatchstep.asdf"
-
-    [rtdata.get_data(f"{l2_data_folder}/{data}") for data in input_data]
-    asnfn = "skymatch_asn.json"
-    rtdata.get_data(f"{l2_data_folder}/{asnfn}")
-    # rtdata.get_truth(f"truth/WFI/image/{output_data}")
-
-    rtdata.input = asnfn
-    rtdata.output = output_data
+    rtdata.get_asn("WFI/image/L3_regtest_asn.json")
+    rtdata.output = "skymatch_test"
 
     args = [
         "romancal.step.SkyMatchStep",
         rtdata.input,
         f"--output_file='{rtdata.output}'",
+        "--subtract=True",
     ]
     RomanStep.from_cmdline(args)
 
-    diff = compare_asdf(rtdata.output, rtdata.truth, **ignore_asdf_paths)
+    step = SkyMatchStep()
+    suffix = step.default_suffix()
 
-    assert diff.identical, diff.report()
+    n_members = len(rtdata.asn["products"][0]["members"])
+    # create a list with the output filenames
+    output_filenames = [f"{rtdata.output}_{i}_{suffix}.asdf" for i in range(n_members)]
+
+    # instantiate a ModelLibrary object
+    library = ModelLibrary(output_filenames)
+    with library:
+        for index, model in enumerate(library):
+            # Perform DMS tests
+            step.log.info(
+                "DMSXXX MSG: SkyMatchStep added meta.background? :"
+                f'  {hasattr(model.meta, "background")}'
+            )
+            assert hasattr(model.meta, "background")
+
+            step.log.info(
+                "DMSXXX MSG: SkyMatchStep populated meta.background? :"
+                f"  {all(is_not_none_or_empty(v) for v in model.meta.background.values())}"
+            )
+            assert all(is_not_none_or_empty(v) for v in model.meta.background.values())
+
+            library.shelve(model, index)
+
+
+@metrics_logger()
+@pytest.mark.bigdata
+def test_l3_output_contains_background_info(rtdata):
+    """Test for the presence of meta.background in L3 files."""
+
+    rtdata.get_asn("WFI/image/L3_regtest_asn.json")
+    rtdata.output = "test_background_info"
+
+    args = [
+        "roman_mos",
+        rtdata.input,
+        f"--output_file='{rtdata.output}'",
+    ]
+    MosaicPipeline.from_cmdline(args)
+
+    step = MosaicPipeline()
+    suffix = step.default_suffix()
+
+    n_members = len(rtdata.asn["products"][0]["members"])
+    # create a list with the output filenames
+    output_filenames = [f"{rtdata.output}_{i}_{suffix}.asdf" for i in range(n_members)]
+
+    # instantiate a ModelLibrary object
+    library = ModelLibrary(output_filenames)
+    with library:
+        for index, model in enumerate(library):
+            # Perform DMS tests
+            step.log.info(
+                "DMSXXX MSG: SkyMatchStep added meta.background? :"
+                f'  {hasattr(model.meta, "background")}'
+            )
+            assert hasattr(model.meta, "background")
+
+            step.log.info(
+                "DMSXXX MSG: SkyMatchStep populated meta.background? :"
+                f"  {all(is_not_none_or_empty(v) for v in model.meta.background.values())}"
+            )
+            assert all(is_not_none_or_empty(v) for v in model.meta.background.values())
+
+            library.shelve(model, index)
