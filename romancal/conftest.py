f64cdcc0a60b59cabfd1712a9b438dd5e76441ea
zacharyburnett <zburnett@stsci.edu>
2024-09-12 11:50:56 -0400

    scan source directory instead of using pytest

--- romancal/conftest.py
+++ romancal/conftest.py
@@ -19,25 +19,6 @@ from romancal.assign_wcs import pointing
 
 collect_ignore = ["lib/dqflags.py"]
 
-ALL_TEST_NAMES = set()
-
-
-def pytest_collection_modifyitems(config, items):
-    for item in items:
-
-        if not hasattr(item, "module"):
-            # for doctests just use name (since it contains the module)
-            name = item.name
-        else:
-            # otherwise add module
-            name = ".".join((item.module.__name__, item.name))
-        ALL_TEST_NAMES.add(name)
-
-
-@pytest.fixture
-def all_test_names():
-    return ALL_TEST_NAMES
-
 
 @pytest.fixture
 def mk_tmp_dirs():
