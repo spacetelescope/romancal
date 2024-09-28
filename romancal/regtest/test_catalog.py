528d38cb075b9e785ead59a7628240e60cd264ea
Brett <brettgraham@gmail.com>
2024-09-28 15:40:25 -0400

    refactor test_catalog

--- romancal/regtest/test_catalog.py
+++ romancal/regtest/test_catalog.py
@@ -3,58 +3,27 @@
 import asdf
 import pytest
 
-from romancal.source_catalog.source_catalog_step import SourceCatalogStep
 from romancal.stpipe import RomanStep
 
 
-def passfail(bool_expr):
-    """set pass fail"""
-    if bool_expr:
-        return "Pass"
-    return "Fail"
-
-
-def check_catalog_fields(model, log, modeltype):
-    fields = model.dtype.names
-    has_pos = ("ra_centroid" in fields) and ("dec_centroid" in fields)
-    log.info(
-        f"DMS374 MSG: {modeltype} Catalog contains sky coordinates of sources? :"
-        + passfail(has_pos)
-    )
-
-    has_flux = "aper_total_flux" in fields
-    log.info(f"DMS375 MSG: {modeltype} Catalog contains fluxes? :" + passfail(has_flux))
-
-    has_type = "is_extended" in fields
-    log.info(
-        f"DMS376 MSG: {modeltype} Catalog contains source classification? :"
-        + passfail(has_type)
-    )
-
-    has_flux_err = "aper_total_flux_err" in fields
-    log.info(
-        f"DMS386 MSG: {modeltype} Catalog contains flux uncertainties? :"
-        + passfail(has_flux_err)
-    )
-
-    has_flags = "flags" in fields
-    log.info(
-        f"DMS387 MSG: {modeltype} Catalog contains DQ flags? :" + passfail(has_flags)
-    )
-
-    return has_pos & has_flux & has_type & has_flux_err & has_flags
-
-
 @pytest.mark.bigdata
 @pytest.mark.soctests
-def test_catalog_l3(rtdata, ignore_asdf_paths):
-    # DMS374: positions on ICRF
-    # DMS375: fluxes
-    # DMS376: type of source
-    # DMS386: flux uncertainties
-    # DMS387: DQ flags
-    inputfn = "r0099101001001001001_F158_visit_i2d.asdf"
-    outputfn = inputfn.replace("_i2d", "_cat")
+@pytest.fixture(
+    scope="module",
+    params=[
+        "r0099101001001001001_F158_visit_i2d.asdf",
+        "r0000101001001001001_01101_0001_WFI01_cal.asdf",
+    ],
+    ids=["L3", "L2"],
+)
+def run_source_catalog(rtdata_module, request):
+    rtdata = rtdata_module
+
+    inputfn = request.param
+
+    outputfn = inputfn.rsplit("_", 1)[0] + "_cat.asdf"
+    rtdata.output = outputfn
+
     rtdata.get_data(f"WFI/image/{inputfn}")
     rtdata.input = inputfn
     rtdata.get_truth(f"truth/WFI/image/{outputfn}")
@@ -64,36 +33,30 @@ def test_catalog_l3(rtdata, ignore_asdf_paths):
         rtdata.input,
     ]
     RomanStep.from_cmdline(args)
-    catalogfp = asdf.open(outputfn)
-    catalog = catalogfp["roman"]["source_catalog"]
-    step = SourceCatalogStep()
-    assert check_catalog_fields(catalog, step.log, "L3")
-
-    # no compare_asdf on the catalogs
+    return rtdata_module
 
 
-@pytest.mark.bigdata
-@pytest.mark.soctests
-def test_catalog_l2(rtdata, ignore_asdf_paths):
-    # DMS374: positions on ICRF
-    # DMS375: fluxes
-    # DMS376: type of source
-    # DMS386: flux uncertainties
-    # DMS387: DQ flags
-    inputfn = "r0000101001001001001_01101_0001_WFI01_cal.asdf"
-    outputfn = inputfn.replace("cal", "cat")
-    rtdata.get_data(f"WFI/image/{inputfn}")
-    rtdata.input = inputfn
-    rtdata.get_truth(f"truth/WFI/image/{outputfn}")
+@pytest.fixture(scope="module")
+def catalog(run_source_catalog):
+    with asdf.open(run_source_catalog.output) as af:
+        yield af["roman"]["source_catalog"]
 
-    args = [
-        "romancal.step.SourceCatalogStep",
-        rtdata.input,
-    ]
-    RomanStep.from_cmdline(args)
-    catalogfp = asdf.open(outputfn)
-    catalog = catalogfp["roman"]["source_catalog"]
-    step = SourceCatalogStep()
 
-    assert check_catalog_fields(catalog, step.log, "L2")
-    # no compare_asdf on the catalogs
+@pytest.fixture(scope="module")
+def fields(catalog):
+    return catalog.dtype.names
+
+
+@pytest.mark.parametrize(
+    "field",
+    (
+        "ra_centroid",  # DMS374 positions on ICRF
+        "dec_centroid",  # DMS374 positions on ICRF
+        "aper_total_flux",  # DMS375 fluxes
+        "is_extended",  # DMS376 type of source
+        "aper_total_flux_err",  # DMS386 flux uncertainties
+        "flags",  # DMS387 dq_flags
+    ),
+)
+def test_has_field(fields, field):
+    assert field in fields
