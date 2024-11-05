e95988ced666551b1f289a7b282df6ac81e26ff5
Eddie Schlafly <eschlafly@stsci.edu>
2024-11-05 15:08:44 -0500

    Updating logging and asserts.

--- romancal/regtest/test_catalog.py
+++ romancal/regtest/test_catalog.py
@@ -4,6 +4,7 @@ import asdf
 import pytest
 
 from romancal.stpipe import RomanStep
+from romancal.source_catalog.source_catalog_step import SourceCatalogStep
 
 # mark all tests in this module
 pytestmark = [pytest.mark.bigdata, pytest.mark.soctests]
@@ -13,6 +14,17 @@ input_filenames = [
     "r0000101001001001001_0001_wfi01_cal.asdf",
 ]
 
+field_list = [
+        "ra_centroid",  # DMS374 positions on ICRF
+        "dec_centroid",  # DMS374 positions on ICRF
+        "aper_total_flux",  # DMS375 fluxes
+        "aper30_flux",  # DMS399 aperture fluxes
+        "aper50_flux",  # DMS399 aperture fluxes
+        "aper70_flux",  # DMS399 aperture fluxes
+        "is_extended",  # DMS376 type of source
+        "aper_total_flux_err",  # DMS386 flux uncertainties
+        "flags",  # DMS387 dq_flags
+]
 
 @pytest.fixture(
     scope="module",
@@ -51,18 +63,7 @@ def fields(catalog):
 
 
 @pytest.mark.parametrize(
-    "field",
-    (
-        "ra_centroid",  # DMS374 positions on ICRF
-        "dec_centroid",  # DMS374 positions on ICRF
-        "aper_total_flux",  # DMS375 fluxes
-        "aper30_flux",  # DMS399 aperture fluxes
-        "aper50_flux",  # DMS399 aperture fluxes
-        "aper70_flux",  # DMS399 aperture fluxes
-        "is_extended",  # DMS376 type of source
-        "aper_total_flux_err",  # DMS386 flux uncertainties
-        "flags",  # DMS387 dq_flags
-    ),
+    "field", field_list,
 )
 def test_has_field(fields, field):
     assert field in fields
@@ -70,9 +71,11 @@ def test_has_field(fields, field):
 
 def test_deblend_source_catalog(rtdata_module):
     rtdata = rtdata_module
-    inputfn = input_filenames[1]
+    inputfn = input_filenames[1]  # use L2 image; could have picked either one
     rtdata.get_data(f"WFI/image/{inputfn}")
     rtdata.input = inputfn
+
+    step = SourceCatalogStep()
 
     outputfn1 = inputfn.rsplit("_", 1)[0] + "_cat.asdf"
     args = ["romancal.step.SourceCatalogStep", rtdata.input,
@@ -88,26 +91,25 @@ def test_deblend_source_catalog(rtdata_module):
     af2 = asdf.open(outputfn2)
     nsrc1 = len(af1['roman']['source_catalog'])
     nsrc2 = len(af2['roman']['source_catalog'])
-    from romancal.source_catalog.source_catalog_step import SourceCatalogStep
-    step = SourceCatalogStep()
     step.log.info(
         "DMS393: Deblended source catalog contains more sources "
         f"({nsrc2}) than undeblended ({nsrc1})?  "
         + ('PASS' if nsrc2 > nsrc1 else 'FAIL'))
-    print('hello!')
-    # can't figure out where these log messages are going??
     assert nsrc2 > nsrc1
-    # what's the nicer way to do this with fixtures?
     
 
 def test_kernel_detection(rtdata_module):
     rtdata = rtdata_module
-    inputfn = input_filenames[1]
+    inputfn = input_filenames[1]  # use L2 image; could have picked either one
     rtdata.get_data(f"WFI/image/{inputfn}")
     rtdata.input = inputfn
     outputfn = inputfn.rsplit("_", 1)[0] + "_kernel10_cat.asdf"
+    step = SourceCatalogStep()
     args = ["romancal.step.SourceCatalogStep", rtdata.input,
-            "--kernel_fwhm", "20", "--output_file", outputfn]
+            "--kernel_fwhm", "10", "--output_file", outputfn]
     RomanStep.from_cmdline(args)
     af = asdf.open(outputfn)
-    # test_has_fields on this catalog with the large kernel?
+    fields = af['roman']['source_catalog'].dtype.names
+    for field in field_list:
+        assert field in field_list
+    step.log.info("DMS391: Used alternative kernel to detect sources.  PASS")
