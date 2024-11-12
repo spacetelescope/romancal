0e709a9ef299193bd6ce75212f93f97350df3686
Larry Bradley <larry.bradley@gmail.com>
2024-11-12 12:51:30 -0500

    Include err==0 in the background coverage mask

--- romancal/multiband_catalog/background.py
+++ romancal/multiband_catalog/background.py
@@ -6,12 +6,28 @@ from romancal.source_catalog.background import RomanBackground
 
 
 def subtract_background(model, box_size=1000):
+    """
+    Subtract the background from the input model.
+
+    Parameters
+    ----------
+    model : ImageModel or MosaicModel
+        The input model to subtract the background from.
+
+    box_size : int
+        The size of the box to use for the background estimation.
+
+    Returns
+    -------
+    model : ImageModel or MosaicModel
+        The input model with the background subtracted.
+    """
     if not isinstance(model, (ImageModel, MosaicModel)):
         raise ValueError("The input model must be an ImageModel or MosaicModel.")
 
     # Subtract the background
     mask = np.isnan(model.data)
-    coverage_mask = np.isnan(model.err)
+    coverage_mask = np.logical_or(np.isnan(model.err), model.err == 0)
     bkg = RomanBackground(
         model.data,
         box_size=box_size,
@@ -23,6 +39,22 @@ def subtract_background(model, box_size=1000):
 
 
 def subtract_background_library(library, box_size=1000):
+    """
+    Subtract the background from all models in the input library.
+
+    Parameters
+    ----------
+    library : ModelLibrary
+        The input library to subtract the background from.
+
+    box_size : int
+        The size of the box to use for the background estimation.
+
+    Returns
+    -------
+    library : ModelLibrary
+        The input library with the background subtracted.
+    """
     if not isinstance(library, ModelLibrary):
         raise TypeError("library input must be a ModelLibrary object")
 
