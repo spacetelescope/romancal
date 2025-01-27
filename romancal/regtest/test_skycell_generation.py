f12325b7c8f52e03dbfa4176d7be0d893e79ca0a
Brett <brettgraham@gmail.com>
2025-01-27 09:26:49 -0500

    only single json encode skycell wcs in asn

--- romancal/regtest/test_skycell_generation.py
+++ romancal/regtest/test_skycell_generation.py
@@ -1,15 +1,28 @@
 """Roman tests for generating associatinos based on skycells"""
 
+import json
 import os
 
 import pytest
 
 from romancal.associations import skycell_asn
 
+# mark all tests in this module
+pytestmark = [pytest.mark.bigdata]
 
-@pytest.mark.bigdata
-def test_skycell_asn_generation(rtdata):
-    """Test for the generation of associations based on skycells"""
+EXPECTED_FILENAMES = [
+    "r512_p_v01001001001_r274dp63x31y80_f158_asn.json",
+    "r512_p_v01001001001_r274dp63x31y81_f158_asn.json",
+    "r512_p_v01001001001_r274dp63x32y82_f158_asn.json",
+    "r512_p_v01001001001_r274dp63x32y80_f158_asn.json",
+    "r512_p_v01001001001_r274dp63x32y81_f158_asn.json",
+    "r512_p_v01001001001_r274dp63x32y82_f158_asn.json",
+]
+
+
+@pytest.fixture(scope="module")
+def run_skycell_asn(rtdata_module):
+    rtdata = rtdata_module
 
     # This test should generate seven json files
     args = [
@@ -22,17 +35,17 @@ def test_skycell_asn_generation(rtdata):
     rtdata.get_data("WFI/image/r0000101001001001001_0002_wfi10_cal.asdf")
 
     skycell_asn._cli(args)
+    return rtdata
 
-    # skycell associations that should be generated
-    output_files = [
-        "r512_p_v01001001001_r274dp63x31y80_f158_asn.json",
-        "r512_p_v01001001001_r274dp63x31y81_f158_asn.json",
-        "r512_p_v01001001001_r274dp63x32y82_f158_asn.json",
-        "r512_p_v01001001001_r274dp63x32y80_f158_asn.json",
-        "r512_p_v01001001001_r274dp63x32y81_f158_asn.json",
-        "r512_p_v01001001001_r274dp63x32y82_f158_asn.json",
-    ]
-    # Test that the json files exist
-    for file in output_files:
-        skycell_asn.logger.info(f"Check that the json file exists{file}")
-        assert os.path.isfile(file)
+
+@pytest.mark.parametrize("expected_filename", EXPECTED_FILENAMES)
+def test_file_exists(run_skycell_asn, expected_filename):
+    """Test that the expected json files were generated"""
+    assert os.path.isfile(expected_filename)
+
+
+@pytest.mark.parametrize("expected_filename", EXPECTED_FILENAMES)
+def test_files_contain_wcsinfo(run_skycell_asn, expected_filename):
+    with open(expected_filename) as f:
+        asn = json.load(f)
+    assert "skycell_wcs_info" in asn
