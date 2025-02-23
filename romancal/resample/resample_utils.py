277712e8f984d633899cb02cecac8d19eab0ed84
Brett <brettgraham@gmail.com>
2025-02-23 04:58:12 -0500

    remove build_driz_weight

--- romancal/resample/resample_utils.py
+++ romancal/resample/resample_utils.py
@@ -1,9 +1,6 @@
 import logging
-import warnings
 
 import numpy as np
-from astropy.nddata.bitmask import bitfield_to_boolean_mask
-from roman_datamodels.dqflags import pixel
 from stcal.alignment.util import wcs_from_footprints
 
 from romancal.assign_wcs.utils import wcs_bbox_from_shape
@@ -93,79 +90,3 @@ def make_output_wcs(
         )
 
     return output_wcs
-
-
-def build_driz_weight(
-    model,
-    weight_type=None,
-    good_bits: str | None = None,
-):
-    """
-    Builds the drizzle weight map for resampling.
-
-    Parameters
-    ----------
-    model : object
-        The input model.
-    weight_type : str, optional
-        The type of weight to use. Allowed values are 'ivm' or 'exptime'.
-        Defaults to None.
-    good_bits : str, optional
-        The good bits to use for building the mask. Defaults to None.
-
-    Returns
-    -------
-    numpy.ndarray
-        The drizzle weight map.
-
-    Raises
-    ------
-    ValueError
-        If an invalid weight type is provided.
-
-    Examples
-    --------
-    .. code-block:: none
-
-        model = get_input_model()
-        weight_map = build_driz_weight(model, weight_type='ivm', good_bits=[1, 2, 3])
-        print(weight_map)
-    """
-
-    dqmask = bitfield_to_boolean_mask(
-        model.dq,
-        good_bits,
-        good_mask_value=1,
-        dtype=np.uint8,
-        flag_name_map=pixel,
-    )
-
-    if weight_type == "ivm":
-        if (
-            hasattr(model, "var_rnoise")
-            and model.var_rnoise is not None
-            and model.var_rnoise.shape == model.data.shape
-        ):
-            with np.errstate(divide="ignore", invalid="ignore"):
-                inv_variance = model.var_rnoise**-1
-            inv_variance[~np.isfinite(inv_variance)] = 1
-        else:
-            warnings.warn(
-                "var_rnoise array not available. Setting drizzle weight map to 1",
-                RuntimeWarning,
-                stacklevel=2,
-            )
-            inv_variance = 1.0
-        result = inv_variance * dqmask
-    elif weight_type == "exptime":
-        exptime = model.meta.exposure.exposure_time
-        result = exptime * dqmask
-    elif weight_type is None:
-        result = np.ones(model.data.shape, dtype=model.data.dtype) * dqmask
-    else:
-        raise ValueError(
-            f"Invalid weight type: {weight_type}."
-            "Allowed weight types are 'ivm' or 'exptime'."
-        )
-
-    return result.astype(np.float32)
