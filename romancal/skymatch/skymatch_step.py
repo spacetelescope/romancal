090d6071bdaba42886541701acc29927bafded6e
Brett <brettgraham@gmail.com>
2025-04-03 09:17:37 -0400

    remove maker_utils from skymatch

--- romancal/skymatch/skymatch_step.py
+++ romancal/skymatch/skymatch_step.py
@@ -10,7 +10,6 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 from astropy.nddata.bitmask import bitfield_to_boolean_mask, interpret_bit_flags
-from roman_datamodels import maker_utils
 from roman_datamodels.dqflags import pixel
 from stcal.skymatch import SkyImage, SkyStats, skymatch
 
@@ -107,9 +106,7 @@ class SkyMatchStep(RomanStep):
         input_image_model = image_model
 
         if "background" not in image_model.meta:
-            image_model.meta["background"] = maker_utils.mk_sky_background(
-                level=None, subtracted=None, method="None"
-            )
+            image_model.meta["background"] = {}
 
         if self._dqbits is None:
             dqmask = np.isfinite(image_model.data).astype(dtype=np.uint8)
