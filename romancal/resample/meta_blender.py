84d006109cb6af766902ea98a1c08c6590cfe54d
Brett <brettgraham@gmail.com>
2025-04-08 13:28:26 -0400

    remove append_individual_meta

--- romancal/resample/meta_blender.py
+++ romancal/resample/meta_blender.py
@@ -1,8 +1,50 @@
+from collections import defaultdict
+
 import numpy as np
-from roman_datamodels import datamodels, maker_utils
+from asdf.lazy_nodes import AsdfDictNode, AsdfListNode
+from astropy.table import QTable
+from roman_datamodels import datamodels, maker_utils, stnode
+
+
+class MissingCellType:
+    pass
+
+
+MISSING_CELL = MissingCellType()
+
+
+class TableBuilder:
+    def __init__(self):
+        self._columns = {}
+        self._nrows = 0
+
+    def _get_column(self, name):
+        if name not in self._columns:
+            self._columns[name] = [MISSING_CELL] * self._nrows
+        return self._columns[name]
+
+    def _santize_value(self, value):
+        if isinstance(value, list | dict | AsdfDictNode | AsdfListNode):
+            return str(value)
+        return value
+
+    def add_row(self, data_dict):
+        updated = set()
+        for key, value in data_dict.items():
+            self._get_column(key).append(self._santize_value(value))
+            updated.add(key)
+        for key in self._columns.keys() - updated:
+            self._columns[key].append(MISSING_CELL)
+
+    def to_table(self):
+        # TODO fix None and MISSING_CELL values
+        return QTable(self._columns)
 
 
 class MetaBlender:
+    def __init__(self):
+        self._tables = defaultdict(lambda: TableBuilder())
+
     def _blend_first(self, model):
         # make a blank mosic metdata node
         self._model = maker_utils.mk_datamodel(
@@ -36,6 +78,19 @@ class MetaBlender:
                     getattr(model.meta.cal_step, step_name),
                 )
 
+    def _update_tables(self, meta):
+        basic_data = {}
+        for key, value in meta.to_flat_dict().items():
+            if key in {"wcs", "cal_logs"}:
+                continue
+
+            if isinstance(value, stnode.DNode):
+                self._tables[key].add_row(value)
+            else:
+                basic_data[key] = value
+        if basic_data:
+            self._tables["basic"].add_row(basic_data)
+
     def blend(self, model):
         if not hasattr(self, "_model"):
             self._blend_first(model)
@@ -57,15 +112,11 @@ class MetaBlender:
         )
         self._mid_mjds.append(mid_time)
 
-        # FIXME this is an existing hack that we have to reproduce here
-        # since roman_datamodels was never updated
-        cal_logs = model.meta.cal_logs
-        del model.meta["cal_logs"]
-        try:
-            self._model.append_individual_image_meta(model.meta)
-        finally:
-            model.meta.cal_logs = cal_logs
+        self._update_tables(model.meta)
 
     def finalize(self):
         self._meta.basic.time_mean_mjd = np.mean(self._mid_mjds)
+        self._meta.individual_image_meta = stnode.IndividualImageMeta()
+        for table_name, builder in self._tables.items():
+            self._meta.individual_image_meta[table_name] = builder.to_table()
         return self._model
