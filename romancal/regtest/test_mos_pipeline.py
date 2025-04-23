dc601254509ff474e333a516ea1d870a9bf1ef9b
zacharyburnett <zburnett@stsci.edu>
2025-04-23 15:43:58 -0400

    use GWCS object instead of FITS WCS

--- romancal/regtest/test_mos_pipeline.py
+++ romancal/regtest/test_mos_pipeline.py
@@ -6,7 +6,7 @@ import pytest
 import roman_datamodels as rdm
 
 from romancal.pipeline.mosaic_pipeline import MosaicPipeline
-from romancal.skycell.skymap import wcsinfo_to_gwcs
+from romancal.skycell.skymap import wcsinfo_to_wcs
 
 from . import util
 from .regtestdata import compare_asdf
@@ -127,7 +127,7 @@ def test_added_background_level(output_model):
 
 def test_wcsinfo_wcs_roundtrip(output_model):
     """Test that the contents of wcsinfo reproduces the wcs"""
-    gwcs_from_wcsinfo = wcsinfo_to_gwcs(output_model.meta.wcsinfo)
+    wcs_from_wcsinfo = wcsinfo_to_wcs(output_model.meta.wcsinfo)
 
-    ra_mad, dec_mad = util.comp_wcs_grids_arcs(output_model.meta.wcs, gwcs_from_wcsinfo)
+    ra_mad, dec_mad = util.comp_wcs_grids_arcs(output_model.meta.wcs, wcs_from_wcsinfo)
     assert (ra_mad + dec_mad) / 2.0 < 1.0e-5
