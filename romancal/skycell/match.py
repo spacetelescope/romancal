fix rotation and add more test cases

romancal/skycell/match.py
@@ -183,15 +183,35 @@ def find_skycell_matches(
     intersecting_skycell_indices = []
 
     # query the global k-d tree of projection regions for possible intersection candidates in (normalized) 3D space
-    _, nearby_projregion_indices = sc.SKYMAP.projregions_kdtree.query(
+    _, nearby_projregion_indices = sc.SKYMAP.projection_regions_kdtree.query(
         footprint.vectorpoint_center,
         k=footprint.possibly_intersecting_projregions,
-        distance_upper_bound=footprint.length / 2 + sc.ProjectionRegion.MAX_LENGTH,
     )
 
+    # # find the maximum distance from the footprint center to neighboring projection region centers
+    # footprint_cartesian_length = sga.length(*footprint.vectorpoint_corners[[0, 2]])
+    # max_projregion_cartesian_length = (
+    #     0.08174916691321579  # Cartesian length of the longest projection region
+    # )
+    # max_projregion_cartesian_distance = (
+    #     footprint_cartesian_length + max_projregion_cartesian_length
+    # ) / 2
+    #
+    # # find Cartesian distances from each projection region center to the footprint center
+    # projregion_vectorpoints = sgv.lonlat_to_vector(
+    #     sc.SKYMAP.projection_regions["ra_tangent"],
+    #     sc.SKYMAP.projection_regions["dec_tangent"],
+    # )
+    # distances = projregion_vectorpoints - footprint.vectorpoint_center[:, None]
+    #
+    # # cull Cartesian distances by a radius defined by the longest projection region and the current footprint
+    # nearby_projregion_indices = np.unique(
+    #     np.where(distances < max_projregion_cartesian_distance * 1.1)[0]
+    # )
+
     for projregion_index in nearby_projregion_indices:
-        # filter out missing neighbors
-        if projregion_index == sc.SKYMAP.projregions_kdtree.n:
+        # filter out missing neighbors from the k-d tree query
+        if projregion_index == len(sc.SKYMAP.projection_regions):
             continue
 
         projregion = sc.ProjectionRegion(projregion_index)
@@ -200,16 +220,34 @@ def find_skycell_matches(
             _, projregion_nearby_skycell_indices = projregion.skycells_kdtree.query(
                 footprint.vectorpoint_center,
                 k=footprint.possibly_intersecting_skycells,
-                distance_upper_bound=footprint.length / 2 + sc.SkyCell.length,
             )
+
+            # # find the maximum distance from the footprint center to neighboring skycell centers
+            # max_skycell_cartesian_length = 3.290522520333678e-05
+            # max_skycell_cartesian_distance = (
+            #     footprint_cartesian_length + max_skycell_cartesian_length
+            # ) / 2
+            #
+            # # find Cartesian distances from each skycell to the footprint center
+            # skycell_vectorpoints = sgv.lonlat_to_vector(
+            #     projregion.skycells["ra_center"],
+            #     projregion.skycells["dec_center"],
+            # )
+            # distances = skycell_vectorpoints - footprint.vectorpoint_center[:, None]
+            #
+            # # cull Cartesian distances by a radius defined by the average skycell and the current footprint
+            # projregion_nearby_skycell_indices = np.unique(
+            #     np.where(distances < max_skycell_cartesian_distance * 1.1)[0]
+            # )
 
             # convert to absolute indices over the full skycell table
             projregion_nearby_skycell_indices = [
                 projregion.data["skycell_start"] + index
                 for index in projregion_nearby_skycell_indices
-                # filter out missing neighbors
-                if index != projregion.skycells_kdtree.n
+                # filter out missing neighbors from the k-d tree query
+                if index != len(projregion.skycells)
             ]
+
             nearby_skycell_indices.extend(projregion_nearby_skycell_indices)
 
             # find polygons that intersect the image footprint

romancal/skycell/plot.py
@@ -27,10 +27,10 @@ def find_intersecting_projregions(
     """
 
     # find the closest projection regions to the image center
-    _, nearby_projregion_indices = sc.SKYMAP.projregions_kdtree.query(
+    _, nearby_projregion_indices = sc.SKYMAP.projection_regions_kdtree.query(
         footprint.vectorpoint_center,
         k=footprint.possibly_intersecting_projregions,
-        distance_upper_bound=footprint.length / 2 + sc.ProjectionRegion.MAX_LENGTH,
+        distance_upper_bound=footprint.length + sc.ProjectionRegion.MAX_LENGTH,
     )
 
     intersecting_projregion_indices = []
@@ -111,7 +111,8 @@ def plot_skycell(
     skycell: sc.SkyCell,
     tangent_vectorpoint: tuple[float, float, float],
     color=None,
-    label: bool = True,
+    label: str | None = None,
+    annotation: str | None = None,
     axis=None,
 ):
     if axis is None:
@@ -125,12 +126,14 @@ def plot_skycell(
         [corners_tangentplane, corners_tangentplane[None, -1]]
     )
 
-    axis.plot(corners_tangentplane[:, 0], corners_tangentplane[:, 1], color=color)
+    axis.plot(
+        corners_tangentplane[:, 0], corners_tangentplane[:, 1], color=color, label=label
+    )
 
-    if label:
+    if annotation:
         center = np.mean(corners_tangentplane[:-1], axis=0)
         axis.annotate(
-            skycell.name, center, va="center", ha="center", size=10, color=color
+            annotation, center, va="center", ha="center", size=10, color=color
         )
 
 
@@ -157,7 +160,7 @@ def plot_image_footprint_and_skycells(
         figure.gca().invert_xaxis()
         figure.suptitle(f"projection region {projregion_index}")
         axis = figure.subplots(1, 1)
-        axis.plot(0, 0, "*", markersize=10)
+        axis.plot(0, 0, "+", markersize=10)
 
         projregion = sc.ProjectionRegion(projregion_index)
 
@@ -178,13 +181,12 @@ def plot_image_footprint_and_skycells(
                 skycell,
                 tangent_vectorpoint,
                 color="darkgrey",
-                label=False,
             )
 
         _, nearby_skycell_indices = projregion.skycells_kdtree.query(
             footprint.vectorpoint_center,
             k=footprint.possibly_intersecting_skycells,
-            distance_upper_bound=footprint.length / 2 + sc.SkyCell.length,
+            distance_upper_bound=footprint.length + sc.SkyCell.length,
         )
 
         for skycell_index in nearby_skycell_indices:
@@ -193,14 +195,11 @@ def plot_image_footprint_and_skycells(
                 skycell,
                 tangent_vectorpoint,
                 color="red",
-                label=False,
             )
 
             if footprint.polygon.intersects_poly(skycell.polygon):
                 plot_skycell(
-                    skycell,
-                    tangent_vectorpoint,
-                    color="blue",
+                    skycell, tangent_vectorpoint, color="blue", annotation=skycell.name
                 )
 
         axis.set_xlabel("Offset from nearest tangent point in arcsec")

romancal/skycell/tests/test_skycell.py
@@ -9,6 +9,12 @@ from numpy.testing import assert_allclose
 import romancal.skycell.skymap as sc
 
 
+def assert_allclose_lonlat(actual: np.ndarray, desired: np.ndarray, rtol=1e-7, atol=0):
+    assert_allclose(
+        np.array(actual) % 360, np.array(desired) % 360, rtol=rtol, atol=atol
+    )
+
+
 @pytest.fixture(autouse=True)
 def override_skymap(monkeypatch):
     """
@@ -100,33 +106,21 @@ def test_skycell_wcs_pixel_to_world(name):
     wcs = skycell.wcs
 
     # forward transform to radec corners
-    assert_allclose(
+    assert_allclose_lonlat(
         np.array(
             wcs(
                 *np.array(
                     [
-                        (0.0, 0.0),
-                        (skycell.pixel_shape[0] - 1, 0.0),
-                        (skycell.pixel_shape[0] - 1, skycell.pixel_shape[1] - 1),
-                        (0.0, skycell.pixel_shape[1] - 1),
+                        (0.5, 0.5),
+                        (skycell.pixel_shape[0] + 0.5, 0.5),
+                        (skycell.pixel_shape[0] + 0.5, skycell.pixel_shape[1] + 0.5),
+                        (0.5, skycell.pixel_shape[1] + 0.5),
                     ]
                 ).T
             )
         ).T,
         skycell.radec_corners,
-    )
-
-    # inverse transform to pixel corners
-    assert_allclose(
-        np.array(wcs.invert(*skycell.radec_corners.T)).T,
-        (
-            [
-                (0.0, 0.0),
-                (skycell.pixel_shape[0] - 1, 0.0),
-                (skycell.pixel_shape[0] - 1, skycell.pixel_shape[1] - 1),
-                (0.0, skycell.pixel_shape[1] - 1),
-            ]
-        ),
+        rtol=1e-5,
     )
 
 
@@ -151,12 +145,13 @@ def test_skycell_wcs_world_to_pixel(name):
         np.array(wcs.invert(*skycell.radec_corners.T)).T,
         (
             [
-                (0.0, 0.0),
-                (skycell.pixel_shape[0] - 1, 0.0),
-                (skycell.pixel_shape[0] - 1, skycell.pixel_shape[1] - 1),
-                (0.0, skycell.pixel_shape[1] - 1),
+                (0.5, 0.5),
+                (skycell.pixel_shape[0] + 0.5, 0.5),
+                (skycell.pixel_shape[0] + 0.5, skycell.pixel_shape[1] + 0.5),
+                (0.5, skycell.pixel_shape[1] + 0.5),
             ]
         ),
+        rtol=1e-5,
     )
 
 
@@ -177,25 +172,34 @@ def test_skycell_wcsinfo(name):
     wcs = skycell.wcs
     wcsinfo = skycell.wcsinfo
 
-    assert_allclose(
-        wcs(wcsinfo["x_ref"], wcsinfo["y_ref"]), (wcsinfo["ra_ref"], wcsinfo["dec_ref"])
+    assert_allclose_lonlat(
+        wcs(wcsinfo["x_ref"], wcsinfo["y_ref"]),
+        (wcsinfo["ra_ref"], wcsinfo["dec_ref"]),
+        rtol=1e-5,
     )
-    assert_allclose(
-        wcs(wcsinfo["pixel_shape"][0] / 2.0, wcsinfo["pixel_shape"][1] / 2.0),
+    assert_allclose_lonlat(
+        wcs(
+            (wcsinfo["pixel_shape"][0] / 2.0) + 0.5,
+            (wcsinfo["pixel_shape"][1] / 2.0) + 0.5,
+        ),
         (wcsinfo["ra_center"], wcsinfo["dec_center"]),
+        rtol=1e-5,
     )
 
-    assert_allclose(
+    assert_allclose_lonlat(
         np.array(
             wcs(
-                np.array(
+                *np.array(
                     [
-                        (0.0, 0.0),
-                        (0.0, wcsinfo["pixel_shape"][1]),
-                        (wcsinfo["pixel_shape"][0], wcsinfo["pixel_shape"][1]),
-                        (wcsinfo["pixel_shape"][0], 0.0),
+                        (0.5, 0.5),
+                        (wcsinfo["pixel_shape"][0] + 0.5, 0.5),
+                        (
+                            wcsinfo["pixel_shape"][0] + 0.5,
+                            wcsinfo["pixel_shape"][1] + 0.5,
+                        ),
+                        (0.5, wcsinfo["pixel_shape"][1] + 0.5),
                     ]
-                )
+                ).T
             )
         ).T,
         (
@@ -206,4 +210,5 @@ def test_skycell_wcsinfo(name):
                 (wcsinfo["ra_corn4"], wcsinfo["dec_corn4"]),
             ]
         ),
+        rtol=1e-5,
     )