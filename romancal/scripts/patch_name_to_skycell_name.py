c204683b359585d0ba788b658c92c082df570222
zacharyburnett <zburnett@stsci.edu>
2025-05-09 16:41:43 -0400

    move script and add disclaimer to remove it when no longer needed

--- romancal/scripts/patch_name_to_skycell_name.py
+++ romancal/scripts/patch_name_to_skycell_name.py
@@ -1,3 +1,10 @@
+"""
+convert old patch names (i.e. `r274dp63x31y80`) from the old patch table
+to the names of the closest sky cells (i.e. `270p65x48y69`) from the new sky cell table
+
+remove this script when we no longer have any old files with old patch names
+"""
+
 from pathlib import Path
 
 import asdf
@@ -14,7 +21,30 @@ def patch_names_to_skycell_names(
     patch_table_path: Path = PATCH_TABLE_PATH,
     skymap: sc.SkyMap = sc.SKYMAP,
 ) -> list[str]:
-    patch_table = asdf.open(patch_table_path)
+    """
+    convert old patch names (i.e. `r274dp63x31y80`) from the old patch table
+    to the names of the closest sky cells (i.e. `270p65x48y69`) from the new sky cell table
+
+    Parameters
+    ----------
+    patch_names : list[str]
+        list of patch names, i.e. `r274dp63x31y80`
+    patch_table_path : Path
+        path to patch table; defaults to /grp/roman/scsb/tesselation/patches.asdf
+    skymap : romancal.skycell.skymap.SkyMap
+        SkyMap instance; defaults to global `skycells` table from CRDS
+
+    Returns
+    -------
+    list of sky cell names, i.e. `270p65x48y69`
+
+    Examples
+    --------
+    >>> patch_names_to_skycell_names(["r274dp63x31y80", "r274dp63x31y81", "r274dp63x32y82", "r274dp63x32y80", "r274dp63x32y81", "r274dp63x32y82"])
+    ['225p90x49y67', '225p90x49y69', '225p90x50y70', '225p90x50y67', '225p90x50y69', '225p90x50y70']
+    """
+
+    patch_table = asdf.open(patch_table_path, memmap=True)
 
     skycell_names = []
     projregions = {}
