b222c6b61208a7bafaa5d559665baeb0210af01b
Brett <brettgraham@gmail.com>
2025-05-13 15:02:26 -0400

    add fake prd and sdf versions

--- romancal/resample/meta_blender.py
+++ romancal/resample/meta_blender.py
@@ -85,6 +85,8 @@ class MetaBlender:
         self._model = datamodels.MosaicModel.from_schema(
             {
                 "meta": {
+                    "prd_version": "8.8.8",
+                    "sdf_software_version": "7.7.7",
                     "file_date": Time(
                         "2020-01-01T00:00:00.0", format="isot", scale="utc"
                     ),
