e506d3490fecc2a5e1006c64103754c2c8a7fbb8
mairan <mteodoro@stsci.edu>
2025-05-16 09:37:40 -0400

    Update romancal/resample/resample.py
    Co-authored-by: Brett Graham <brettgraham@gmail.com>

--- romancal/resample/resample.py
+++ romancal/resample/resample.py
@@ -9,7 +9,7 @@ from romancal.patch_match.patch_match import to_skycell_wcs
 from .exptime_resampler import ExptimeResampler
 from .l3_wcs import assign_l3_wcs
 from .meta_blender import MetaBlender
-from .resample_utils import add_var_sky_array, make_output_wcs
+from .resample_utils import compute_var_sky, make_output_wcs
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
