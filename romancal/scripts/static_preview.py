10ea6c5a16abcadbb024d541c2d0580d98dd8c82
zacharyburnett <zburnett@stsci.edu>
2025-06-05 15:07:47 -0400

    remove `typer` dependency from `stpreview` by using builtin `argparse` instead

--- romancal/scripts/static_preview.py
+++ romancal/scripts/static_preview.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 
 import asdf
@@ -6,9 +7,6 @@ import numpy
 
 def command():
     try:
-        from typing import Annotated
-
-        import typer
         from stpreview.downsample import downsample_asdf_to
         from stpreview.image import (
             north_pole_angle,
@@ -20,89 +18,75 @@ def command():
             'SDP requirements not installed; do `pip install "romancal[sdp]"`'
         ) from err
 
-    app = typer.Typer()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("INPUT", type=Path, help="path to ASDF file with 2D image data")
+    parser.add_argument("OUTPUT", type=Path, help="path to output image file")
 
-    @app.command()
-    def preview(
-        input: Annotated[
-            Path, typer.Argument(help="path to ASDF file with 2D image data")
-        ],
-        output: Annotated[
-            Path | None, typer.Argument(help="path to output image file")
-        ] = None,
-        shape: Annotated[
-            tuple[int, int] | None,
-            typer.Argument(help="desired pixel resolution of output image"),
-        ] = (1080, 1080),
-        compass: Annotated[
-            bool | None,
-            typer.Option(help="whether to draw a north arrow on the image"),
-        ] = True,
-    ):
-        """
-        create a preview image with a north arrow overlay indicating orientation
-        """
+    subparsers = parser.add_subparsers(dest="subcommand")
 
-        if output is None:
-            output = Path.cwd()
-        if output.is_dir():
-            output = output / f"{input.stem}.png"
+    preview_parser = subparsers.add_parser(
+        "preview", help="downsample the given ASDF image by the given integer factor"
+    )
+    preview_parser.add_argument(
+        "SHAPE",
+        type=int,
+        nargs="+",
+        default=(1080, 1080),
+        help="desired pixel shape of output image",
+    )
+    preview_parser.add_argument(
+        "--no-compass",
+        action="store_true",
+        help="do not draw a north arrow on the image",
+    )
 
-        with asdf.open(input) as file:
-            model = file["roman"]["meta"]["model_type"]
-            if "image" not in model.lower() and "mosaic" not in model.lower():
-                raise NotImplementedError(f'"{model}" model not supported')
-            wcs = file["roman"]["meta"]["wcs"]
+    thumbnail_parser = subparsers.add_parser(
+        "thumbnail",
+        help="downsample the given ASDF image to the desired shape (the output image may be smaller than the desired shape, if no even factor exists)",
+    )
+    thumbnail_parser.add_argument(
+        "SHAPE",
+        type=int,
+        nargs="+",
+        default=(300, 300),
+        help="desired pixel shape of output image",
+    )
+    thumbnail_parser.add_argument(
+        "--compass",
+        action="store_true",
+        help="draw a north arrow on the image",
+    )
 
-        data = downsample_asdf_to(input=input, shape=shape, func=numpy.nanmean)
+    arguments = parser.parse_args()
 
-        write_image(
-            data,
-            output,
-            shape=shape,
-            normalization=percentile_normalization(data, percentile=90),
-            colormap="afmhot",
-            north_arrow_angle=north_pole_angle(wcs).degree - 90,
+    output = arguments.OUTPUT
+    if output is None:
+        output = Path.cwd()
+    if output.is_dir():
+        output = (
+            output / f"{input.stem}.png"
+            if arguments.subcommand == "preview"
+            else f"{input.stem}_thumb.png"
         )
 
-    @app.command()
-    def thumbnail(
-        input: Annotated[
-            Path, typer.Argument(help="path to ASDF file with 2D image data")
-        ],
-        output: Annotated[
-            Path | None, typer.Argument(help="path to output image file")
-        ] = None,
-        shape: Annotated[
-            tuple[int, int] | None,
-            typer.Argument(help="desired pixel resolution of output image"),
-        ] = (300, 300),
-        compass: Annotated[
-            bool | None,
-            typer.Option(help="whether to draw a north arrow on the image"),
-        ] = False,
-    ):
-        if output is None:
-            output = Path.cwd()
-        if output.is_dir():
-            output = output / f"{input.stem}_thumb.png"
+    with asdf.open(arguments.INPUT) as file:
+        model = file["roman"]["meta"]["model_type"]
+        if "image" not in model.lower() and "mosaic" not in model.lower():
+            raise NotImplementedError(f'"{model}" model not supported')
+        wcs = file["roman"]["meta"]["wcs"]
 
-        with asdf.open(input) as file:
-            model = file["roman"]["meta"]["model_type"]
-            if "image" not in model.lower() and "mosaic" not in model.lower():
-                raise NotImplementedError(f'"{model}" model not supported')
+    data = downsample_asdf_to(input=input, shape=arguments.SHAPE, func=numpy.nanmean)
 
-        data = downsample_asdf_to(input=input, shape=shape, func=numpy.nanmean)
-
-        write_image(
-            data,
-            output,
-            shape=shape,
-            normalization=percentile_normalization(data, percentile=90),
-            colormap="afmhot",
-        )
-
-    app()
+    write_image(
+        data,
+        output,
+        shape=arguments.SHAPE,
+        normalization=percentile_normalization(data, percentile=90),
+        colormap="afmhot",
+        north_arrow_angle=north_pole_angle(wcs).degree - 90
+        if arguments.subcommand == "preview"
+        else None,
+    )
 
 
 if __name__ == "__main__":
