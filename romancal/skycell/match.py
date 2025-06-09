399414ec2de70e17c00b92498d7becb7dfcdd50b
zacharyburnett <zburnett@stsci.edu>
2025-06-09 16:04:56 -0400

    use module

--- romancal/skycell/match.py
+++ romancal/skycell/match.py
@@ -13,7 +13,7 @@ import spherical_geometry.polygon as sgp
 import spherical_geometry.vector as sgv
 from gwcs import WCS
 from numpy.typing import NDArray
-from stcal.alignment import wcs_bbox_from_shape
+from stcal.alignment.util import wcs_bbox_from_shape
 
 import romancal.skycell.skymap as sc
 from romancal.assign_wcs.utils import wcs_pixel_shape_from_bbox
