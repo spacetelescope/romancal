ef8ae89a9b224c5a9fb4957a6e2245c78cde1543
Dave Davis <ddavis@stsci.edu>
2025-07-02 14:42:35 -0400

    rcal-1095 First pass at the dark step refactoring

--- romancal/dark_current/dark_current_step.py
+++ romancal/dark_current/dark_current_step.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+import numpy as np
 
 from roman_datamodels import datamodels as rdm
 
@@ -53,11 +54,24 @@ class DarkCurrentStep(RomanStep):
             if "nresultants" not in dark_model.meta.exposure:
                 dark_model.meta.exposure["nresultants"] = dark_model.data.shape[0]
 
+            # get the dark slope and dark slope error from the reference file & trim ref pixels
+            dark_slope = dark_model.dark_slope[4:-4, 4:-4]
+            dark_slope_err = dark_model.dark_slope_error[4:-4, 4:-4]
+
+
+            # get the resultant info from the input file
+            read_pattern = input_model.meta.exposure.read_pattern
+            frame_time = input_model.meta.exposure.frame_time
+
+            # get the average time for each resultant
+            time_resultants =  np.array([np.average(x) for x in read_pattern], dtype=np.float32) * frame_time
+            # get the time for the dark file
+            time_dark = np.sum(time_resultants)
+
             # Do the dark correction
             out_model = input_model
-            nresultants = len(input_model.meta.exposure["read_pattern"])
-            out_model.data -= dark_model.data[:nresultants]
-            out_model.pixeldq |= dark_model.dq
+            out_model.data -= dark_slope * time_dark
+            out_model.err = np.sqrt( out_model.err **2 + (dark_slope_err * time_dark) **2)
             out_model.meta.cal_step.dark = "COMPLETE"
 
             # Save dark data to file
