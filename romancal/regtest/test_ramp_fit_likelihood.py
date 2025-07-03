64f06b41637df0785e8792640c40f95203b9fa75
Brett <brettgraham@gmail.com>
2025-07-03 09:43:23 -0400

    update likelihood regtest to use dms_logger

--- romancal/regtest/test_ramp_fit_likelihood.py
+++ romancal/regtest/test_ramp_fit_likelihood.py
@@ -3,14 +3,15 @@
 import pytest
 import roman_datamodels as rdm
 
-from romancal.step import RampFitStep
 from romancal.stpipe import RomanStep
 
 from .regtestdata import compare_asdf
 
 
 @pytest.mark.bigdata
-def test_liklihood_rampfit(rtdata, ignore_asdf_paths, resource_tracker, request):
+def test_liklihood_rampfit(
+    rtdata, ignore_asdf_paths, resource_tracker, request, dms_logger
+):
     """Testing ramp fitting  using the likelihood method"""
 
     input_data = "r0000101001001001001_0001_wfi01_f158_darkcurrent.asdf"
@@ -18,11 +19,9 @@ def test_liklihood_rampfit(rtdata, ignore_asdf_paths, resource_tracker, request)
     rtdata.input = input_data
 
     # Define step (for running and log access)
-    step = RampFitStep()
+    dms_logger.info("Testing ramp fitting with the likelihood algorithm. ")
 
-    step.log.info("Testing ramp fitting with the likelihood algorithm. ")
-
-    step.log.info(f"Image data file: {rtdata.input.rsplit('/', 1)[1]}")
+    dms_logger.info(f"Image data file: {rtdata.input.rsplit('/', 1)[1]}")
 
     # Test Likelihood ramp fitting
     output = "r0000101001001001001_0001_wfi01_f158_like_rampfit.asdf"
@@ -32,13 +31,13 @@ def test_liklihood_rampfit(rtdata, ignore_asdf_paths, resource_tracker, request)
         rtdata.input,
         "--output_file=r0000101001001001001_0001_wfi01_f158_like_rampfit.asdf",
     ]
-    step.log.info("Testing the likelihood fitting for ramps")
+    dms_logger.info("Testing the likelihood fitting for ramps")
     with resource_tracker.track(log=request):
         RomanStep.from_cmdline(args)
 
     ramp_results = rdm.open(rtdata.output)
 
-    step.log.info(
+    dms_logger.info(
         "RampFit step recorded as complete? :"
         f" {ramp_results.meta.cal_step.ramp_fit == 'COMPLETE'}"
     )
@@ -46,5 +45,5 @@ def test_liklihood_rampfit(rtdata, ignore_asdf_paths, resource_tracker, request)
 
     rtdata.get_truth(f"truth/WFI/image/{output}")
     diff = compare_asdf(rtdata.output, rtdata.truth, **ignore_asdf_paths)
-    step.log.info(f"Was the rate image data produced? : {diff.identical}")
+    dms_logger.info(f"Was the rate image data produced? : {diff.identical}")
     assert diff.identical, diff.report()
