e1e76e7e5b4f7dfc588a0057e1830809fbfd3c10
Brett <brettgraham@gmail.com>
2025-07-03 09:45:31 -0400

    add docstring

--- romancal/conftest.py
+++ romancal/conftest.py
@@ -32,6 +32,12 @@ def slow(request):
 
 @pytest.fixture(scope="session")
 def dms_logger():
+    """Set up a 'DMS' logger for use in tests.
+
+    The primary use is to report DMS requirement log
+    messages to stderr but can also be used for general
+    stderr logging in tests.
+    """
     logger = logging.getLogger("DMS")
     # Don't propagate to root logger to avoid double reporting
     # during stpipe API calls (like Step.call).
