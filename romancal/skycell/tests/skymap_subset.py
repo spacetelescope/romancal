a54ebbd4e11235f52d11a452a2e23430e39a35a3
Nadia Dencheva <nadia.dencheva@gmail.com>
2025-07-28 16:02:46 -0400

    fix association

--- romancal/skycell/tests/skymap_subset.py
+++ romancal/skycell/tests/skymap_subset.py
@@ -8,9 +8,12 @@ DATA_DIRECTORY = Path(__file__).parent / "data"
 if __name__ == "__main__":
     skymap_subset = skymap.SKYMAP.model.copy()
 
+    # to maintain the proper indices, the subset must contain all the
+    # previous projection regions up to the specified index
     skymap_subset.projection_regions = skymap.SKYMAP.model.projection_regions[
         : LAST_PROJREGION_INDEX + 1
     ].copy()
+
     skymap_subset.skycells = skymap.SKYMAP.model.skycells[
         : skymap_subset.projection_regions[-1]["skycell_end"] + 1
     ].copy()
