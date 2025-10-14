d957e4c955fc9764f49a964cd6f05c64e5b413d8
zacharyburnett <zburnett@stsci.edu>
2025-10-14 14:12:01 -0400

    remove unecessary index handling for `.query_ball_point`

--- romancal/skycell/skymap.py
+++ romancal/skycell/skymap.py
@@ -825,7 +825,6 @@ class ProjectionRegion:
                 for skycell in (
                     SkyCell(self.data["skycell_start"] + skycell_index)
                     for skycell_index in skycell_indices
-                    if skycell_index != len(self.skycell_indices)
                 )
                 if skycell.polygon.contains_point(vectorpoints[point_index, :])
             ]
@@ -974,8 +973,7 @@ class SkyMap:
             [
                 ProjectionRegion(projregion_index)
                 for projregion_index in projregion_indices
-                if projregion_index != len(self.model.projection_regions)
-                and ProjectionRegion(projregion_index).polygon.contains_point(
+                if ProjectionRegion(projregion_index).polygon.contains_point(
                     vectorpoints[vectorpoint_index]
                 )
             ]
@@ -1011,7 +1009,6 @@ class SkyMap:
                 for skycell in (
                     SkyCell(skycell_index)
                     for skycell_index in skycell_indices
-                    if skycell_index != len(SKYMAP.model.skycells)
                 )
                 if skycell.polygon.contains_point(vectorpoints[point_index, :])
             ]
