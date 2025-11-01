9956653ae338966ce0ea2866d1e258889fdd4c9b
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2025-11-01 13:10:49 -0400

    update update_wcs api for new defaults

--- romancal/orientation/set_telescope_pointing.py
+++ romancal/orientation/set_telescope_pointing.py
@@ -358,11 +358,13 @@ def add_wcs(
 
         t_pars, transforms = update_wcs(
             model,
+            allow_default=allow_default,
             default_pa_v3=default_pa_v3,
+            default_quaternion=default_quaternion,
+            default_target_v2v3=default_target_v2v3,
+            reduce_func=reduce_func,
             service_kwargs=service_kwargs,
             tolerance=tolerance,
-            allow_default=allow_default,
-            reduce_func=reduce_func,
             **transform_kwargs,
         )
 
@@ -380,12 +382,14 @@ def add_wcs(
 
 def update_wcs(
     model,
+    allow_default=False,
     default_pa_v3=0.0,
+    default_quaternion=(None,)*4,
     default_roll_ref=0.0,
+    default_target_v2v3=(None,)*2,
+    reduce_func=None,
     service_kwargs=None,
     tolerance=60,
-    allow_default=False,
-    reduce_func=None,
     **transform_kwargs,
 ):
     """
@@ -402,9 +406,28 @@ def update_wcs(
     model : `~roman.datamodels.DataModel`
         The model to update.
 
+    allow_default : bool
+        If telemetry cannot be determine, use existing
+        information in the observation's header.
+
+    default_pa_v3 : float
+        The V3 position angle to use if the pointing information
+        is not found.
+
+    default_quaternion : (int, int, int, int)
+        The quaternion 4-tuple: (q1, q2, q3, q4).
+        Used if no engineering data is available.
+
     default_roll_ref : float
         If pointing information cannot be retrieved,
         use this as the roll ref angle.
+
+    default_v2v3 : (float, float)
+        Target V2/V3 position, (v2, v3), in the telescopes field of view in arcsec.
+        Used if no engineering data is available.
+
+    reduce_func : func or None
+        Reduction function to use on values.
 
     service_kwargs : dict or None
         Keyword arguments passed to `engdb_service` defining what
@@ -414,13 +437,6 @@ def update_wcs(
         If no telemetry can be found during the observation,
         the time, in seconds, beyond the observation time to
         search for telemetry.
-
-    allow_default : bool
-        If telemetry cannot be determine, use existing
-        information in the observation's header.
-
-    reduce_func : func or None
-        Reduction function to use on values.
 
     **transform_kwargs : dict
         Keyword arguments used by matrix calculation routines.
