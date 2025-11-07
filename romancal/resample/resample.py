6b2fb09db1e25500b68936d3aaa6864af59c4ed7
Mairan Teodoro <mteodoro@stsci.edu>
2025-11-07 11:24:44 -0500

    Add default parameter for 'data_release_id'

--- romancal/resample/resample.py
+++ romancal/resample/resample.py
@@ -278,7 +278,7 @@ class ResampleData(Resample):
             "target", "None"
         )
         # get data release ID
-        output_model.meta.data_release_id = self.input_models.asn.get("data_release_id")
+        output_model.meta.data_release_id = self.input_models.asn.get("data_release_id", "p")
 
         return output_model
 
