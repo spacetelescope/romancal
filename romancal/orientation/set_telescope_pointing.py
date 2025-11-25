5175273b4df9d7a590aa34e219aa58fd022f452c
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2025-11-25 15:27:55 -0500

    add commanded guide star position to cli api

--- romancal/orientation/set_telescope_pointing.py
+++ romancal/orientation/set_telescope_pointing.py
@@ -229,6 +229,8 @@ class TransformParameters:
     aperture: str = ""
     #: Default quaternion to use if engineering is not available.
     default_quaternion: tuple | None = None
+    #: Commanded position of the guide star in (H, V) space.
+    gscommanded: tuple | None = None
     #: Corrected, or apparent, sky location of the guide star.
     gspos: tuple | None = None
     #: Observation end time
