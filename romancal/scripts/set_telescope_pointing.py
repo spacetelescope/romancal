cc582fb2b68ad7ebc1e04c04ece0d544913d8815
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2025-11-25 15:27:55 -0500

    add commanded guide star position to cli api

--- romancal/scripts/set_telescope_pointing.py
+++ romancal/scripts/set_telescope_pointing.py
@@ -42,6 +42,14 @@ def main():
         help="Perform all actions but do not save the results",
     )
     parser.add_argument(
+        '--gscommanded',
+        type=float,
+        nargs=2,
+        metavar=('H', 'V'),
+        default=None,
+        help='Commanded position of the guide start in (H,V) space'
+    )
+    parser.add_argument(
         '--gspos',
         type=float,
         nargs=2,
@@ -155,6 +163,7 @@ def main():
                 # set_telescope_pointing.TransformParameters
                 allow_default=args.allow_default,
                 default_quaternion=args.default_quaternion,
+                gscommanded=args.gscommanded,
                 gspos=args.gspos,
                 service_kwargs=service_kwargs,
                 tolerance=args.tolerance,
