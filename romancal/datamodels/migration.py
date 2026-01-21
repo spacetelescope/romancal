6494d21ab34cf90aaff9a16d0d058ec71e589b50
Paul Huwe <42071634+PaulHuwe@users.noreply.github.com>
2026-01-21 12:42:10 -0500

    RCAL-1314: Add WFI_parallel Keyword Migration (#2143)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- romancal/datamodels/migration.py
+++ romancal/datamodels/migration.py
@@ -30,6 +30,14 @@ def update_model_version(model, *, close_on_update=False):
     if type(updated_model.meta.file_date) is not Time:
         updated_model.meta.file_date = Time(updated_model.meta.file_date)
 
+    # old files (<B21) lacks WFI_parallel
+    if ("wfi_parallel" not in model.meta.get("observation", {})) and (
+        visit_file_sequence := model.meta.get("observation", {}).get(
+            "visit_file_sequence", None
+        )
+    ):
+        updated_model.meta.observation.wfi_parallel = visit_file_sequence > 1
+
     if close_on_update:
         model.close()
 
