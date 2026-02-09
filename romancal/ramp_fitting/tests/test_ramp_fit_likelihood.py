55e810b895df6d3556fd4ccaa95c5364886c592a
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2026-02-09 17:58:44 -0500

    don't be fancy; just skip test

--- romancal/ramp_fitting/tests/test_ramp_fit_likelihood.py
+++ romancal/ramp_fitting/tests/test_ramp_fit_likelihood.py
@@ -80,7 +80,7 @@ def test_bad_readpattern():
         )
 
 
-@pytest.mark.xfail(sys.platform == "linux", reason="See rcal-1324")
+@pytest.mark.skip(reason='See rcal-1324')
 def test_flag_large_events_withsnowball():
     """Test that large events are flagged"""
     resultants = create_linear_ramp(n_resultants=20, nrows=100, ncols=100)
