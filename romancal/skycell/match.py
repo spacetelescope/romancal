f5070bd5c693d6374feeaefdae7bbbefc9dae701
zacharyburnett <zburnett@stsci.edu>
2026-02-18 08:55:39 -0500

    offload footprint construction to `SphericalPolygon.from_wcs()`

--- romancal/skycell/match.py
+++ romancal/skycell/match.py
@@ -56,56 +56,18 @@ class ImageFootprint:
         image footprint object
         """
 
-        if not hasattr(wcs, "bounding_box") or wcs.bounding_box is None:
-            raise ValueError(
-                "Cannot infer image footprint from WCS without a bounding box."
-            )
-
-        array_shape = (
-            wcs.array_shape
-            if hasattr(wcs, "array_shape") and wcs.array_shape is not None
-            else tuple(
-                wcs.bounding_box[index][1] - wcs.bounding_box[index][0]
-                for index in range(len(wcs.bounding_box))
-            )
-        )
-        if extra_vertices_per_edge <= 0:
+        if (
+            extra_vertices_per_edge <= 0
+            and hasattr(wcs, "bounding_box")
+            and wcs.bounding_box is not None
+        ):
             vertex_points = wcs.footprint(center=False)
         else:
-            # constrain number of vertices to the maximum number of pixels on an edge, excluding the corners
-            if extra_vertices_per_edge > max(array_shape) - 2:
-                extra_vertices_per_edge = max(array_shape) - 2
-
-            # build a list of pixel indices that represent equally-spaced edge vertices
-            vertices_per_edge = 2 + extra_vertices_per_edge
-            origin_indices = np.zeros(vertices_per_edge - 1) - 0.5
-            x_end_indices = array_shape[0] - origin_indices
-            y_end_indices = array_shape[1] - origin_indices
-            vertices_x = np.linspace(
-                0, array_shape[0], num=vertices_per_edge - 1, endpoint=False
-            )
-            vertices_y = np.linspace(
-                0, array_shape[1], num=vertices_per_edge - 1, endpoint=False
-            )
-            edge_indices = np.concatenate(
-                [
-                    # north edge
-                    np.stack([origin_indices, vertices_y], axis=1),
-                    # east edge
-                    np.stack([vertices_x, y_end_indices], axis=1),
-                    # south edge
-                    np.stack([x_end_indices, y_end_indices - vertices_y], axis=1),
-                    # west edge
-                    np.stack([x_end_indices - vertices_x, origin_indices], axis=1),
-                ],
-                axis=0,
-            )
-
-            # query the WCS for pixel indices at the edges
-            vertex_points = np.stack(
-                wcs(*edge_indices.T, with_bounding_box=False),
-                axis=1,
-            )
+            vertex_points = np.array(
+                sgp.SingleSphericalPolygon.from_wcs(
+                    wcs, steps=extra_vertices_per_edge + 1
+                ).to_lonlat()
+            ).T
 
         return cls(vertex_points)
 
@@ -123,7 +85,7 @@ class ImageFootprint:
     def vectorpoint_vertices(self) -> NDArray[float]:
         """vertices in 3D Cartesian space on the unit sphere"""
         return sgv.normalize_vector(
-            np.stack(sgv.lonlat_to_vector(*np.array(self.radec_corners).T), axis=1)
+            np.stack(sgv.lonlat_to_vector(*self.radec_corners.T), axis=1)
         )
 
     @cached_property
