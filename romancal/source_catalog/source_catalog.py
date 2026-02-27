5b2eead0975af0954451936592f77bddb55eff8f
Larry Bradley <larry.bradley@gmail.com>
2026-02-27 09:22:58 -0500

    Input mask to SourceCatalog

--- romancal/source_catalog/source_catalog.py
+++ romancal/source_catalog/source_catalog.py
@@ -275,6 +275,7 @@ class RomanSourceCatalog:
             self.model,
             self.segment_img,
             self.convolved_data,
+            self.mask,
             self._pixel_area,
             self._wcs_angle,
             detection_cat=self.detection_cat,
