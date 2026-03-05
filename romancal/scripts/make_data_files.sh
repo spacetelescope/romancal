132f6d7a230b93e5ccd54546716f80a14034c940
Brett <brettgraham@gmail.com>
2026-03-05 14:08:48 -0500

    update script to generate data-workshop files

--- romancal/scripts/make_data_files.sh
+++ romancal/scripts/make_data_files.sh
@@ -21,11 +21,12 @@ set -e
 # set up the directory structure
 mkdir -p $outdir/L1
 mkdir -p $outdir/MOC
+mkdir -p $outdir/data-workshop
 
 # Redirect all output to the logfile and the terminal
 exec > >(tee $logfile) 2>&1
 
-# run elp for 1 uncal for each wfi
+# run elp for 1 uncal image for each wfi
 L1_FNS=`ls r0000101001001001001_0001_wfi*_f158_uncal.asdf`
 for L1_FN in $L1_FNS; do
     cp $L1_FN $outdir/L1/
@@ -36,4 +37,39 @@ done
 L2_FNS=`ls r0000101001001001001_0001_wfi*_f158_cal.asdf`
 for L2_FN in $L2_FNS; do
     cp $L2_FN $outdir/MOC/
+done
+
+# data-workshop files
+# L1 -> L2
+WORKSHOP_UNCALS="
+    r0000101001001001001_0001_wfi01_f158_uncal.asdf
+    r0000101001001001001_0001_wfi01_grism_uncal.asdf
+    r0000101001001001001_0001_wfi01_prism_uncal.asdf
+"
+for WORKSHOP_UNCAL in $WORKSHOP_UNCALS; do
+    if [ $WORKSHOP_UNCAL =~ "_f158_"]; then
+        # image file, already processed through elp to a cal file above
+        cp "${WORKSHOP_UNCAL//_uncal/_cal}" $outdir/data-workshop/
+        cp "${WORKSHOP_UNCAL//_uncal/_wcs}" $outdir/data-workshop/
+        cp "${WORKSHOP_UNCAL//_uncal/_segm}" $outdir/data-workshop/
+        cp "${WORKSHOP_UNCAL//_uncal.asdf/_cat.parquet}" $outdir/data-workshop/
+
+        # prepare associations
+        skycell_asn "${WORKSHOP_UNCAL//_uncal/_cal}" -o r00001 --product-type visit
+    else
+        # grism/prism file, only run through elp
+        strun roman_elp $WORKSHOP_UNCAL
+        cp "${WORKSHOP_UNCAL//_uncal/_cal}" $outdir/data-workshop/
+        cp "${WORKSHOP_UNCAL//_uncal/_wcs}" $outdir/data-workshop/
+    fi
+done
+
+# L2 -> L3
+ASN_FNS=`ls *_asn.json`
+for ASN_FN in $ASN_FNS; do
+    strun roman_mos $ASN_FN
+    cp $ASN_FN $outdir/data-workshop/
+    cp "${WORKSHOP_UNCAL//_asn.json/_coadd.asdf}" $outdir/data-workshop/
+    cp "${WORKSHOP_UNCAL//_asn.json/_cat.parquet}" $outdir/data-workshop/
+    cp "${WORKSHOP_UNCAL//_asn.json/_segm.asdf}" $outdir/data-workshop/
 done
