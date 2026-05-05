7d686a65754a16efded94c94476cc83c6775e138
Brett Graham <brettgraham@gmail.com>
2026-05-05 10:08:00 -0400

    Clean up public API for: lib, outlier_detection, skycell (#2300)

--- romancal/lib/tests/test_suffix.py
+++ romancal/lib/tests/test_suffix.py
@@ -1,34 +1,24 @@
 """Test suffix replacement"""
 
-import logging
-
 import pytest
 
 from romancal.lib import suffix as s
-from romancal.lib.basic_utils import LoggingContext
 
 
-@pytest.fixture(scope="module")
-def enable_logging():
-    with LoggingContext(logging.getLogger(s.__name__), handler=logging.StreamHandler()):
-        yield
-
-
-@pytest.mark.filterwarnings("ignore:romancal.dqflags is deprecated.*")
-def test_suffix_existence(enable_logging):
+def test_suffix_existence():
     """Generate current suffix list and compare"""
 
-    calculated_suffixes = s.find_suffixes()
-    found_suffixes = s.combine_suffixes(
-        to_add=(calculated_suffixes, s.SUFFIXES_TO_ADD),
-        to_remove=(s.SUFFIXES_TO_DISCARD,),
+    calculated_suffixes = s._find_suffixes()
+    found_suffixes = s._combine_suffixes(
+        to_add=(calculated_suffixes, s._SUFFIXES_TO_ADD),
+        to_remove=(s._SUFFIXES_TO_DISCARD,),
     )
-    diff = set(found_suffixes).symmetric_difference(s.KNOW_SUFFIXES)
+    diff = set(found_suffixes).symmetric_difference(s._KNOW_SUFFIXES)
     assert not diff, f"Suffixes unaccounted for:\n{diff}"
 
 
-@pytest.mark.parametrize("suffix", s.KNOW_SUFFIXES)
-def test_suffix_removal(suffix, enable_logging):
+@pytest.mark.parametrize("suffix", s._KNOW_SUFFIXES)
+def test_suffix_removal(suffix):
     """Test suffix removal"""
     basename = "file"
     full_fpath = basename + "_" + suffix
@@ -37,8 +27,8 @@ def test_suffix_removal(suffix, enable_logging):
     assert separator == "_"
 
 
-@pytest.mark.parametrize("suffix", s.KNOW_SUFFIXES)
-def test_suffix_replacement(suffix, enable_logging, base="file", new="junk", sep="_"):
+@pytest.mark.parametrize("suffix", s._KNOW_SUFFIXES)
+def test_suffix_replacement(suffix, base="file", new="junk", sep="_"):
     """Test suffix replacement"""
     full_path = base + sep + suffix
     replaced = s.replace_suffix(full_path, new)
