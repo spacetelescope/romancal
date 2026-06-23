528c31600c3a576c66ffd170928b765f5a19ca30
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2026-06-23 14:10:08 -0400

    remove siaf initialization

--- romancal/orientation/_siaf.py
+++ romancal/orientation/_siaf.py
@@ -13,8 +13,3 @@ def open_siaf(basepath=None, filename=None):
     from pysiaf import Siaf
 
     SIAF = Siaf("roman", basepath=basepath, filename=filename)
-
-
-# Initialize for the default SIAF.
-if SIAF is None:
-    open_siaf()
