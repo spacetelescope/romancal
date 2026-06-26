62fb37424e1136621aa4a0c92813685e714bbcb9
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2026-06-26 07:58:14 -0400

    RCAL-1412: Implement ability to choose local pysiaf xml folder (#2367)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ romancal/orientation/tests/test_siaf.py
@@ -0,0 +1,28 @@
+# Test SIAF access
+from pathlib import Path
+
+import pytest
+
+# pysiaf is not a required dependency. If not present, ignore all this.
+pysiaf = pytest.importorskip("pysiaf")
+
+import romancal.orientation._siaf as siaf_lib  # noqa: E402
+
+
+@pytest.mark.parametrize(
+    "basepath, n_apers",
+    [
+        (None, 22),
+        pytest.param(
+            Path(__file__).parent / "data" / "siaf",
+            5,
+            marks=pytest.mark.xfail(
+                reason="released pysiaf does not support basepath feature", strict=False
+            ),
+        ),
+    ],
+)
+def test_open_siaf(basepath, n_apers):
+    """Test opening siaf"""
+    siaf = siaf_lib.open_siaf(basepath=basepath)
+    assert len(siaf.apertures) == n_apers
