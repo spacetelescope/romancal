98e3c552769be78257b6897d32b6ee79924a14e4
Brett <brettgraham@gmail.com>
2026-07-08 16:49:12 -0400

    remove unused test code

--- romancal/associations/tests/test_level2_basics.py
+++ romancal/associations/tests/test_level2_basics.py
@@ -1,7 +1,6 @@
 """Test basic usage of Level2 associations"""
 
-from romancal.associations import _generate, load_asn
-from romancal.associations._main import Main
+from romancal.associations import _generate
 from romancal.associations.tests.helpers import (
     combine_pools,
     registry_level2_only,
@@ -11,41 +10,12 @@ from romancal.associations.tests.helpers import (
 REGEX_LEVEL2 = r"(?P<path>.+)(?P<type>_cal?)(?P<extension>\..+)"
 
 
-def from_level2_schema():
-    with open(t_path("data/asn_level2.json")) as asn_file:
-        asn = load_asn(asn_file)
-    return [asn]
-
-
 def generate_from_pool(pool_path):
     """Generate associations from pools"""
     rules = registry_level2_only()
     pool = combine_pools(t_path(pool_path))
     asns = _generate(pool, rules)
     return asns
-
-
-def cmd_from_pool(pool_path, args):
-    """Run commandline on pool
-
-    Parameters
-    ---------
-    pool_path: str
-        The pool to run on.
-
-    args: [arg(, ...)]
-        Additional command line arguments in the form `sys.argv`
-    """
-    full_args = [
-        "--dry-run",
-        "-D",
-        "-r",
-        t_path("../lib/_rules_level2.py"),
-        "--ignore-default",
-    ]
-    full_args.extend(args)
-    result = Main(full_args, pool=pool_path)
-    return result
 
 
 def test_level2_productname():
