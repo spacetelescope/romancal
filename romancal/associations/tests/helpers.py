4eaa5c04550cd930e94e63789017a14a895134f0
Brett <brettgraham@gmail.com>
2026-07-08 17:36:50 -0400

    remove noqa cleanup imports

--- romancal/associations/tests/helpers.py
+++ romancal/associations/tests/helpers.py
@@ -1,19 +1,6 @@
 """Helpers for tests."""
 
-# ruff: noqa
 import os
-import re
-from collections import namedtuple
-from contextlib import contextmanager
-from glob import glob
-from tempfile import TemporaryDirectory
-
-import pytest
-from astropy.table import Table, vstack
-
-from romancal.associations import _AssociationPool, _AssociationRegistry, _generate
-from romancal.associations.lib._counter import Counter
-from romancal.associations.lib._utilities import is_iterable
 
 
 def t_path(partial_path):
