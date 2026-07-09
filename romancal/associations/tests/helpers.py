5b1cc4e636eaf35c60a8ac7324ceaaed24dfda7e
Brett <brettgraham@gmail.com>
2026-07-09 11:25:12 -0400

    remove t_path

--- romancal/associations/tests/helpers.py
+++
@@ -1,9 +0,0 @@
-"""Helpers for tests."""
-
-import os
-
-
-def t_path(partial_path):
-    """Construction the full path for test files"""
-    test_dir = os.path.dirname(__file__)
-    return os.path.join(test_dir, partial_path)
--- romancal/associations/tests/test_generate.py
+++ romancal/associations/tests/test_generate.py
@@ -1,12 +1,18 @@
 """Test basic generate operations"""
 
+import importlib.resources
+
+import romancal.associations
 from romancal.associations import load_asn
-from romancal.associations.tests.helpers import t_path
 
 
 def test_unserialize():
     """Test basic unserializing"""
-    asn_file = t_path("data/asn_mosaic.json")
-    with open(asn_file) as asn_fp:
+    with open(
+        importlib.resources.files(romancal.associations)
+        / "tests"
+        / "data"
+        / "asn_mosaic.json"
+    ) as asn_fp:
         asn = load_asn(asn_fp)
     assert isinstance(asn, dict)
