9b332f69ae39b6c754e9bd4473f2d5b36f268580
Brett <brettgraham@gmail.com>
2026-09-25 07:30:27 -0400

    fix multiband catalog names

--- romancal/regtest/compatibility/test_multiband_catalog.py
+++ romancal/regtest/compatibility/test_multiband_catalog.py
@@ -12,10 +12,18 @@ pytestmark = [pytest.mark.bigdata, pytest.mark.soctests]
 def run_multiband_catalog(old_rtdata_module, request, resource_tracker):
     rtdata = old_rtdata_module
 
-    rtdata.get_asn("WFI/image/L3_skycell_mbcat_asn.json")
+    # The filenames changed for B22
+    _, build = rtdata._env.rsplit("_", maxsplit=1)
+    if build <= "B21":
+        input_fn = "L3_skycell_mbcat_asn.json"
+        output_fn = "r00001_p_v01001001001001_270p65x70y49_f158_mbcat_cat.parquet"
+    else:
+        input_fn = "r00001_p_v01001001001001_270p65x70y49_asn.json"
+        output_fn = "r00001_p_v01001001001001_270p65x70y49_cat.parquet"
 
-    outputfn = "r00001_p_v01001001001001_270p65x70y49_f158_mbcat_cat.parquet"
-    rtdata.output = outputfn
+    rtdata.get_asn(f"WFI/image/{input_fn}")
+
+    rtdata.output = output_fn
 
     args = [
         "romancal.step.MultibandCatalogStep",
