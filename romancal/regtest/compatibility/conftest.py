f904cfe89b451dc7bbe07e916c64fbbdb99ebacc
Brett Graham <brettgraham@gmail.com>
2026-09-25 18:58:33 -0400

    Update compatibility tests to include B22.3 and B23 (#2471)

--- romancal/regtest/compatibility/conftest.py
+++ romancal/regtest/compatibility/conftest.py
@@ -1,7 +1,10 @@
 import pytest
 
 
-@pytest.fixture(scope="module", params=["build/26Q1_B20", "build/26Q2_B21"])
+@pytest.fixture(
+    scope="module",
+    params=["build/26Q1_B20", "build/26Q2_B21", "build/26Q3_B22.3", "build/26Q4_B23"],
+)
 def old_rtdata_module(rtdata_module, request):
     rtdata_module._env = request.param
     yield rtdata_module
